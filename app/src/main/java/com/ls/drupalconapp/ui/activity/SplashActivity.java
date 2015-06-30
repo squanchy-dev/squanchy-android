3365d6171e0e99911ce6091d4d381e84063358df
Roman Danylyk <roman.danylyk@lemberg.co.uk>
2015-06-30 10:37:37 +0300

    Updated SplashActivity [touch:#458]

--- app/src/main/java/com/ls/drupalconapp/ui/activity/SplashActivity.java
+++ app/src/main/java/com/ls/drupalconapp/ui/activity/SplashActivity.java
@@ -2,6 +2,7 @@ package com.ls.drupalconapp.ui.activity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 
 import com.ls.drupalconapp.R;
@@ -13,20 +14,35 @@ import com.ls.utils.AnalyticsManager;
 
 public class SplashActivity extends FragmentActivity {
 
+    private static final int SPLASH_DURATION = 1500;
+    private Handler mHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_splash);
-
         AnalyticsManager.sendEvent(this, "Application", R.string.action_open);
-        performSplash();
+        mHandler = new Handler();
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                performSplash();
+            }
+        }, SPLASH_DURATION);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mHandler.removeCallbacksAndMessages(null);
+        super.onDestroy();
     }
 
     private void performSplash() {
-        final UpdatesManager manager = Model.instance().getUpdatesManager();
         new AsyncTask<Void, Void, UpdatesManager>() {
             @Override
             protected UpdatesManager doInBackground(Void... params) {
+                UpdatesManager manager = Model.instance().getUpdatesManager();
                 manager.checkForDatabaseUpdate();
                 return manager;
             }
