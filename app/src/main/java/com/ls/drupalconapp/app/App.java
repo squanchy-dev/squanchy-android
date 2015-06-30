3d1039ac11cc0bcdd70ad3deb6f8711e24fcfcce
Roman Danylyk <roman.danylyk@lemberg.co.uk>
2015-06-30 15:22:43 +0300

    Integrated crashlitics [touch:#464]

--- app/src/main/java/com/ls/drupalconapp/app/App.java
+++ app/src/main/java/com/ls/drupalconapp/app/App.java
@@ -3,9 +3,11 @@ package com.ls.drupalconapp.app;
 import android.app.Application;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 import com.ls.drupal.DrupalClient;
+import com.ls.drupalconapp.BuildConfig;
 import com.ls.drupalconapp.model.AppDatabaseInfo;
 import com.ls.drupalconapp.model.Model;
 import com.ls.drupalconapp.model.PreferencesManager;
@@ -14,6 +16,8 @@ import com.ls.drupalconapp.ui.view.FontHelper;
 import com.ls.http.base.BaseRequest;
 import com.ls.util.image.DrupalImageView;
 
+import io.fabric.sdk.android.Fabric;
+
 public class App extends Application {
     private static Context mContext;
     private static final String PROPERTY_ID = "UA-267362-65";
@@ -21,6 +25,9 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
 
         mContext = getApplicationContext();
 
