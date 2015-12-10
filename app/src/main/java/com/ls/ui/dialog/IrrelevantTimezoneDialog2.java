8d78bc8df78fd040b8526fc65ece0dcd43a5293b
Taras Kunyk <taras.kunyk@lemberg.co.uk>
2015-12-10 11:20:38 +0200

    1. Extra logs removed

--- app/src/main/java/com/ls/ui/dialog/IrrelevantTimezoneDialog2.java
+++ app/src/main/java/com/ls/ui/dialog/IrrelevantTimezoneDialog2.java
@@ -2,7 +2,6 @@ package com.ls.ui.dialog;
 
 import com.ls.drupalcon.R;
 import com.ls.drupalcon.model.PreferencesManager;
-import com.ls.util.L;
 import com.ls.utils.DateUtils;
 
 import android.app.AlertDialog;
@@ -12,7 +11,6 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
