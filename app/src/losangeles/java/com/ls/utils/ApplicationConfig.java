636634461dbfa3c2d2d4ddd965533564fb628f76
roman <romanD@treelineinteractive.com>
2016-02-01 11:26:50 +0200

    Removed los-angeles folder

--- app/src/losangeles/java/com/ls/utils/ApplicationConfig.java
+++
@@ -1,6 +0,0 @@
-package com.ls.utils;
-
-public class ApplicationConfig {
-    public final static String BASE_URL = "http://drupalcon.uat.link/api/v2/";
-    public static final int CACHE_DISK_USAGE_BYTES = 20 * 1024 * 1024;
-}
