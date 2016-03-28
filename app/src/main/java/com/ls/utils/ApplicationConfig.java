b165951956d01580fba3454192160493354f400b
Taras Kunyk <taras.kunyk@lemberg.co.uk>
2016-03-28 15:53:30 +0300

    1. App domain updated

--- app/src/main/java/com/ls/utils/ApplicationConfig.java
+++ app/src/main/java/com/ls/utils/ApplicationConfig.java
@@ -1,7 +1,7 @@
 package com.ls.utils;
 
 public class ApplicationConfig {
-    public final static String BASE_URL = "http://drupalconbarcelona2015.uat.link/api/v2/";
+    public final static String BASE_URL = "http://neworleans2016.drupalcon.uat.link/api/v2/";
     public static final String PROPERTY_ID = "UA-267362-65";
     public static final int CACHE_DISK_USAGE_BYTES = 20 * 1024 * 1024;
 }
