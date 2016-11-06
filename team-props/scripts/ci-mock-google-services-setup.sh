6d953cbcccbec6ee4b0421ddcb194497dd38768a
Francesco Pontillo <francescopontillo@gmail.com>
2016-11-06 10:34:01 +0000

    Change mock package name to match the one on Travis

--- team-props/scripts/ci-mock-google-services-setup.sh
+++ team-props/scripts/ci-mock-google-services-setup.sh
@@ -12,7 +12,7 @@ echo '{
       "client_info": {
         "mobilesdk_app_id": "0",
         "android_client_info": {
-          "package_name": "com.connfa.dummiladisciassette"
+          "package_name": "com.ls.drupalconapp"
         }
       },
       "api_key": [
