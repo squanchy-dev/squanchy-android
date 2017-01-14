f0cd08f0ef32d42b9c9be5ba09c30eea00838282
Sebastiano Poggi <poggos@gmail.com>
2017-01-14 18:10:19 +0000

    Create ScheduleActivity

---
+++ app/src/main/java/com/connfa/schedule/ScheduleActivity.java
@@ -0,0 +1,15 @@
+package com.connfa.schedule;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.connfa.R;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
