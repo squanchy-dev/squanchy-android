7fc8e4e15479e23ff85c65c300e28f777a2db070
Sebastiano Poggi <poggos@gmail.com>
2017-01-27 19:48:27 +0000

    Rename root content container viewgroup

--- app/src/main/java/com/connfa/navigation/NavigationDrawerActivity.java
+++ app/src/main/java/com/connfa/navigation/NavigationDrawerActivity.java
@@ -13,7 +13,7 @@ import com.connfa.navigation.drawer.NavigationDrawer;
 public abstract class NavigationDrawerActivity extends AppCompatActivity {
 
     private NavigationDrawer navigationDrawer;
-    private ViewGroup contentRoot;
+    private ViewGroup container;
 
     @Override
     public final void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
@@ -30,19 +30,19 @@ public abstract class NavigationDrawerActivity extends AppCompatActivity {
         View drawerContents = findViewById(R.id.navigation_drawer);
         navigationDrawer = new NavigationDrawer(drawerLayout, drawerContents);
 
-        contentRoot = (ViewGroup) findViewById(R.id.content_root);
-        inflateActivityContent(contentRoot);
+        container = (ViewGroup) findViewById(R.id.navigation_drawer_main_container);
+        inflateActivityContent(container);
 
         initializeActivity(savedInstanceState);
-    }
-
-    protected final ViewGroup contentRoot() {
-        return contentRoot;
     }
 
     protected abstract void inflateActivityContent(ViewGroup parent);
 
     protected abstract void initializeActivity(Bundle savedInstanceState);
+
+    protected final ViewGroup container() {
+        return container;
+    }
 
     protected abstract Navigator navigate();
 
