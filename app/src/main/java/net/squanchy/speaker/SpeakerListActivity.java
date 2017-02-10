7c216b1db482059e7aa4f3c1c5d400fab0684f0d
quiro91 <lorenzo.quiroli@gmail.com>
2017-02-10 17:00:33 +0100

    add the SpeakerListActivity

---
+++ app/src/main/java/net/squanchy/speaker/SpeakerListActivity.java
@@ -0,0 +1,6 @@
+package net.squanchy.speaker;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SpeakerListActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/net/squanchy/speaker/view/SpeakerViewHolder.java
@@ -0,0 +1,13 @@
+package net.squanchy.speaker.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class SpeakerViewHolder extends RecyclerView.ViewHolder {
+
+    public SpeakerViewHolder(View itemView) {
+        super(itemView);
+    }
+
+
+}
