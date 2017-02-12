01bc59c2d5290ba15c07055c323f90e48707329c
Sebastiano Poggi <poggos@gmail.com>
2017-02-12 17:30:52 +0000

    Rename variables in EventDetailsService

--- app/src/main/java/net/squanchy/eventdetails/EventDetailsService.java
+++ app/src/main/java/net/squanchy/eventdetails/EventDetailsService.java
@@ -1,11 +1,16 @@
 package net.squanchy.eventdetails;
 
+import java.util.List;
+
 import net.squanchy.schedule.domain.view.Event;
 import net.squanchy.service.firebase.FirebaseSquanchyRepository;
 import net.squanchy.service.firebase.model.FirebaseEvent;
+import net.squanchy.service.firebase.model.FirebaseSpeaker;
 import net.squanchy.service.firebase.model.FirebaseSpeakers;
+import net.squanchy.support.lang.Lists;
 
 import io.reactivex.Observable;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.schedulers.Schedulers;
 
 import static net.squanchy.support.lang.Lists.find;
@@ -26,19 +31,33 @@ class EventDetailsService {
         return Observable.combineLatest(
                 eventObservable,
                 speakersObservable,
-                (firebaseEvent, speakerHolder) -> Event.create(
-                        firebaseEvent.eventId,
-                        dayId,
-                        firebaseEvent.name,
-                        firebaseEvent.place,
-                        firebaseEvent.experienceLevel,
-                        map(
-                                map(firebaseEvent.speakers, speakerId -> find(
-                                        speakerHolder.speakers, speaker -> speaker.speakerId.equals(speakerId))
-                                ),
-                                speaker -> speaker == null ? "" : speaker.firstName + " " + speaker.lastName
-                        )
-                )
+                combineIntoEvent(dayId)
         ).subscribeOn(Schedulers.io());
+    }
+
+    private BiFunction<FirebaseEvent, FirebaseSpeakers, Event> combineIntoEvent(int dayId) {
+        return (apiEvent, apiSpeakers) -> {
+            List<FirebaseSpeaker> speakers = speakersForEvent(apiEvent, apiSpeakers);
+            return Event.create(
+                    apiEvent.eventId,
+                    dayId,
+                    apiEvent.name,
+                    apiEvent.place,
+                    apiEvent.experienceLevel,
+                    map(speakers, toSpeakerName())
+            );
+        };
+    }
+
+    private List<FirebaseSpeaker> speakersForEvent(FirebaseEvent apiEvent, FirebaseSpeakers apiSpeakers) {
+        return map(apiEvent.speakers, speakerId -> findSpeaker(apiSpeakers, speakerId));
+    }
+
+    private FirebaseSpeaker findSpeaker(FirebaseSpeakers apiSpeakers, long speakerId) {
+        return find(apiSpeakers.speakers, apiSpeaker -> apiSpeaker.speakerId.equals(speakerId));
+    }
+
+    private Lists.Function<FirebaseSpeaker, String> toSpeakerName() {
+        return apiSpeaker -> apiSpeaker != null ? apiSpeaker.firstName + " " + apiSpeaker.lastName : null;
     }
 }
