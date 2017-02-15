1620a27c76c352c5d147811b7bf954e7ad5e2f49
Sebastiano Poggi <poggos@gmail.com>
2017-02-15 21:16:57 +0000

    Move Speaker model to correct package

--- app/src/main/java/net/squanchy/speaker/SpeakerService.java
+++ app/src/main/java/net/squanchy/speaker/SpeakerService.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 
 import net.squanchy.service.firebase.FirebaseSquanchyRepository;
+import net.squanchy.speaker.domain.view.Speaker;
 
 import io.reactivex.Observable;
 
