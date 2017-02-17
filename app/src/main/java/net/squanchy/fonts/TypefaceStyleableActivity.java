ef148c47b27987728f87e36b53880b15789f296d
Roberto Orgiu <roberto.orgiu@gmail.com>
2017-02-17 16:28:50 +0100

    Formatted Typeface base Activity to better match project defaults and codestyle

--- app/src/main/java/net/squanchy/fonts/TypefaceStyleableActivity.java
+++ app/src/main/java/net/squanchy/fonts/TypefaceStyleableActivity.java
@@ -3,7 +3,7 @@ package net.squanchy.fonts;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 
-public class TypefaceStyleableActivity extends AppCompatActivity{
+public class TypefaceStyleableActivity extends AppCompatActivity {
 
     @Override
     protected void attachBaseContext(Context newBase) {
