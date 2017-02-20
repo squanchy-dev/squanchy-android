b5aeefcdae30d3e0d428cad58e46e623651336a6
Sebastiano Poggi <poggos@gmail.com>
2017-02-20 19:33:00 +0100

    Begin using CardLayout and defining schedule item UI

--- app/src/main/java/net/squanchy/support/widget/CardLayout.java
+++ app/src/main/java/net/squanchy/support/widget/CardLayout.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
+import net.squanchy.R;
+
 public class CardLayout extends FrameLayout {
 
     public CardLayout(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+        this(context, attrs, R.attr.cardViewDefaultStyle);
     }
 
     public CardLayout(Context context, AttributeSet attrs, int defStyleAttr) {
