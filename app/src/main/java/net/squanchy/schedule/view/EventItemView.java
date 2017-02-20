2701245641180d46db630f68058367f693f94104
Sebastiano Poggi <poggos@gmail.com>
2017-02-20 20:15:41 +0100

    Add speaker to schedule items

--- app/src/main/java/net/squanchy/schedule/view/EventItemView.java
+++ app/src/main/java/net/squanchy/schedule/view/EventItemView.java
@@ -2,6 +2,7 @@ package net.squanchy.schedule.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.squanchy.R;
@@ -14,6 +15,8 @@ public class EventItemView extends CardLayout {
     private TextView titleView;
     private TextView timestampView;
     private ExperienceLevelIconView experienceLevelIconView;
+    private ImageView speakerPhotoView;
+    private TextView speakerNameView;
 
     public EventItemView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -30,11 +33,17 @@ public class EventItemView extends CardLayout {
         timestampView = (TextView) findViewById(R.id.timestamp);
         titleView = (TextView) findViewById(R.id.title);
         experienceLevelIconView = (ExperienceLevelIconView) findViewById(R.id.experience_level_icon);
+        speakerPhotoView = (ImageView) findViewById(R.id.speaker_photo);
+        speakerNameView = (TextView) findViewById(R.id.speaker_name);
     }
 
     void updateWith(Event event) {
         timestampView.setText("12:00");         // TODO put start time 
         titleView.setText(event.title());
         experienceLevelIconView.setExperienceLevel(event.experienceLevel());
+
+        speakerPhotoView.setVisibility(event.speakersVisibility());     // TODO bind photo
+        speakerNameView.setVisibility(event.speakersVisibility());
+        speakerNameView.setText(event.speakersNames());
     }
 }
