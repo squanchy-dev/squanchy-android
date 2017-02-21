225da711f1475251c00d3afab01376e88fa722cb
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-02-21 15:14:07 +0100

    create a non swipable ViewPager

---
+++ app/src/main/java/net/squanchy/navigation/NoSwipeViewPager.java
@@ -0,0 +1,23 @@
+package net.squanchy.navigation;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NoSwipeViewPager extends ViewPager {
+
+    public NoSwipeViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return false;
+    }
+}
