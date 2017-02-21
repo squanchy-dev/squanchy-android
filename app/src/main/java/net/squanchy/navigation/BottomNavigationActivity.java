22e6d341565ac74f597dc46da9f874064c1d29a6
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-02-21 17:29:36 +0100

    load the view in the activity

--- app/src/main/java/net/squanchy/navigation/BottomNavigationActivity.java
+++ app/src/main/java/net/squanchy/navigation/BottomNavigationActivity.java
@@ -2,15 +2,44 @@ package net.squanchy.navigation;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 
 import net.squanchy.R;
+import net.squanchy.fonts.TypefaceStyleableActivity;
+import net.squanchy.navigation.view.BottomNavigationPagerAdapter;
 
-public class BottomNavigationActivity extends AppCompatActivity {
+public class BottomNavigationActivity extends TypefaceStyleableActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_view_pager);
+        BottomNavigationPagerAdapter adapter = new BottomNavigationPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                item -> {
+                    switch (item.getItemId()) {
+                        case R.id.action_schedule:
+
+                            break;
+                        case R.id.action_favourite:
+
+                            break;
+                        case R.id.action_tweet:
+
+                            break;
+                        case R.id.action_venue:
+
+                            break;
+                    }
+                    return true;
+                });
     }
 }
