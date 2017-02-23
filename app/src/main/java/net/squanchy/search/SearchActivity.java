478ca27783aae6bdf19ae81cc16af1cb89d8f673
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-02-23 12:07:32 +0100

    create the SearchActivity

---
+++ app/src/main/java/net/squanchy/search/SearchActivity.java
@@ -0,0 +1,14 @@
+package net.squanchy.search;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
