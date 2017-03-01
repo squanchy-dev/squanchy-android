03d3fe773c0e60cac4946471cb47fd6b41686810
Sebastiano Poggi <poggos@gmail.com>
2017-03-01 19:09:31 +0000

    Changed my mind, no fragment. YAGNI anyway

--- app/src/main/java/net/squanchy/navigation/HomeActivity.java
+++ app/src/main/java/net/squanchy/navigation/HomeActivity.java
@@ -1,45 +1,62 @@
 package net.squanchy.navigation;
 
 import android.animation.ValueAnimator;
-import android.content.res.TypedArray;
+import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.AttrRes;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
-import android.support.annotation.StyleRes;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.view.ContextThemeWrapper;
+import android.support.transition.Fade;
+import android.support.transition.TransitionManager;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.squanchy.R;
-import net.squanchy.favorites.FavoritesFragment;
 import net.squanchy.fonts.TypefaceStyleableActivity;
-import net.squanchy.schedule.ScheduleFragment;
-import net.squanchy.tweets.TweetsFragment;
-import net.squanchy.venueinfo.VenueInfoFragment;
 
 public class HomeActivity extends TypefaceStyleableActivity {
 
-    private static final String TAG_CURRENT_SECTION = "current_section";
-    private static final int FRAGMENT_FADE_DURATION = 220;
+    private final Map<BottomNavigationSection, View> pageViews = new HashMap<>(4);
+
+    private int pageFadeDurationMillis;
 
     private BottomNavigationSection currentSection;
+    private BottomNavigationView bottomNavigationView;
+    private ViewGroup pageContainer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        pageFadeDurationMillis = getResources().getInteger(android.R.integer.config_mediumAnimTime);
+
+        pageContainer = (ViewGroup) findViewById(R.id.page_container);
+        collectPageViewsInto(pageViews);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         setupBottomNavigation(bottomNavigationView);
 
         selectPage(BottomNavigationSection.SCHEDULE);
     }
 
+    private void collectPageViewsInto(Map<BottomNavigationSection, View> pageViews) {
+        pageViews.put(BottomNavigationSection.SCHEDULE, pageContainer.findViewById(R.id.schedule_content_root));
+        pageViews.put(BottomNavigationSection.FAVORITES, pageContainer.findViewById(R.id.favorites_content_root));
+        pageViews.put(BottomNavigationSection.TWEETS, pageContainer.findViewById(R.id.tweets_content_root));
+        pageViews.put(BottomNavigationSection.VENUE_INFO, pageContainer.findViewById(R.id.venue_content_root));
+    }
+
     private void setupBottomNavigation(BottomNavigationView bottomNavigationView) {
         BottomNavigationHelper.disableShiftMode(bottomNavigationView);
+        bottomNavigationView.setBackground(bottomNavigationView.getBackground().mutate());
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 item -> {
@@ -69,55 +86,52 @@ public class HomeActivity extends TypefaceStyleableActivity {
             return;
         }
 
+        Fade transition = new Fade();
+        TransitionManager.beginDelayedTransition(pageContainer, transition);
+
+        if (currentSection != null) {
+            pageViews.get(currentSection).setVisibility(View.INVISIBLE);
+        }
+        pageViews.get(section).setVisibility(View.VISIBLE);
+
+        Resources.Theme theme = getThemeFor(section);
+        setStatusBarColor(getColorFromTheme(theme, android.R.attr.statusBarColor));
+        setBottomNavigationBarColor(getColorFromTheme(theme, android.support.design.R.attr.colorPrimary));
+
         currentSection = section;
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-        Fragment currentFragment = fragmentManager.findFragmentByTag(TAG_CURRENT_SECTION);
-        if (currentFragment != null) {
-            transaction.remove(currentFragment);
-        }
-        transaction.add(R.id.fragment_container, createFragmentFor(section), TAG_CURRENT_SECTION)
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                .commit();
-
-        setStatusBarColorFromTheme(section.theme());
     }
 
-    private Fragment createFragmentFor(BottomNavigationSection section) {
-        switch (section) {
-            case SCHEDULE:
-                return new ScheduleFragment();
-            case FAVORITES:
-                return new FavoritesFragment();
-            case TWEETS:
-                return new TweetsFragment();
-            case VENUE_INFO:
-                return new VenueInfoFragment();
-            default:
-                throw new IllegalArgumentException("The section " + section + " is not supported");
-        }
+    private Resources.Theme getThemeFor(BottomNavigationSection section) {
+        Resources.Theme theme = getResources().newTheme();
+        theme.setTo(getTheme());
+        theme.applyStyle(section.theme(), true);
+        return theme;
     }
 
-    private void setStatusBarColorFromTheme(@StyleRes int theme) {
-        ContextThemeWrapper wrappedContext = new ContextThemeWrapper(this, theme);
-        TypedArray a = wrappedContext.obtainStyledAttributes(new int[]{android.R.attr.statusBarColor});
-        try {
-            int statusBarColor = a.getColor(0, 0);
-            setStatusBarColor(statusBarColor);
-        } finally {
-            a.recycle();
-        }
+    @ColorInt
+    private int getColorFromTheme(Resources.Theme theme, @AttrRes int attributeId) {
+        TypedValue typedValue = new TypedValue();
+        theme.resolveAttribute(attributeId, typedValue, true);
+        return typedValue.data;
     }
 
     private void setStatusBarColor(@ColorInt int color) {
         Window window = getWindow();
         int currentStatusBarColor = window.getStatusBarColor();
 
-        ValueAnimator animator = ValueAnimator.ofArgb(currentStatusBarColor, color)
-                .setDuration(FRAGMENT_FADE_DURATION / 2);
-        animator.addUpdateListener(animation -> window.setStatusBarColor((Integer) animation.getAnimatedValue()));
+        animateColor(currentStatusBarColor, color, animation -> window.setStatusBarColor((int) animation.getAnimatedValue()));
+    }
+
+    private void setBottomNavigationBarColor(@ColorInt int color) {
+        ColorDrawable backgroundDrawable = (ColorDrawable) bottomNavigationView.getBackground();
+        int currentBackgroundColor = backgroundDrawable.getColor();
+
+        animateColor(currentBackgroundColor, color, animation -> backgroundDrawable.setColor((int) animation.getAnimatedValue()));
+    }
+
+    private void animateColor(@ColorInt int currentColor, @ColorInt int targetColor, ValueAnimator.AnimatorUpdateListener listener) {
+        ValueAnimator animator = ValueAnimator.ofArgb(currentColor, targetColor).setDuration(pageFadeDurationMillis);
+        animator.addUpdateListener(listener);
         animator.start();
     }
 }
