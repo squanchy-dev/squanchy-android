0f585337167c11d806f1510d8d1dabff91a5c26a
Roberto Orgiu <roberto.orgiu@gmail.com>
2017-03-01 13:12:31 +0100

    Moves the logic for choosing the correct image level from the enum to the View effectively using it

--- app/src/main/java/net/squanchy/eventdetails/domain/view/ExperienceLevel.java
+++ app/src/main/java/net/squanchy/eventdetails/domain/view/ExperienceLevel.java
@@ -1,35 +1,26 @@
 package net.squanchy.eventdetails.domain.view;
 
-import android.support.annotation.IntRange;
 import android.support.annotation.StringRes;
 
 import net.squanchy.R;
 
 public enum ExperienceLevel {
-    BEGINNER("beginner", 0, R.string.experience_level_beginner),
-    INTERMEDIATE("intermediate", 1, R.string.experience_level_intermediate),
-    ADVANCED("advanced", 2,  R.string.experience_level_advanced);
+    BEGINNER("beginner", R.string.experience_level_beginner),
+    INTERMEDIATE("intermediate", R.string.experience_level_intermediate),
+    ADVANCED("advanced", R.string.experience_level_advanced);
 
     private final String rawLevel;
-    private final int imageLevel;
     private final int labelStringResId;
 
     ExperienceLevel(String rawLevel,
-                    @IntRange(from = 0, to = 2) int imageLevel,
                     @StringRes int labelStringResId) {
 
         this.rawLevel = rawLevel;
-        this.imageLevel = imageLevel;
         this.labelStringResId = labelStringResId;
     }
 
     public String rawLevel() {
         return rawLevel;
-    }
-
-    @IntRange(from = 0, to = 2)
-    public int getImageLevel() {
-        return imageLevel;
     }
 
     @StringRes
