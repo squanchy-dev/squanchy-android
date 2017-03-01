506499a5151778c1649817d88beff267508c2515
Sebastiano Poggi <poggos@gmail.com>
2017-03-01 21:16:32 +0000

    Import sources for RenderThread into project
    Not using the remote dependency as we need to tweak the code (but
    will mainline ASAP and switch over)

---
+++ app/src/main/java/me/eugeniomarletti/renderthread/CanvasProperty.java
@@ -0,0 +1,10 @@
+package me.eugeniomarletti.renderthread;
+
+@SuppressWarnings("unused")
+public abstract class CanvasProperty<T> {
+
+    CanvasProperty() {
+    }
+
+    public abstract boolean isHardware();
+}
---
+++ app/src/main/java/me/eugeniomarletti/renderthread/typeannotation/RenderNodeAnimator.java
@@ -0,0 +1,10 @@
+package me.eugeniomarletti.renderthread.typeannotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+public @interface RenderNodeAnimator {
+}
