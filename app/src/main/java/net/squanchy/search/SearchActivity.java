82bffa1d17c41ea55482bbaf333bdad2d4bd9e3e
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-03-02 12:42:08 +0100

    wire everything up in the SearchActivity

--- app/src/main/java/net/squanchy/search/SearchActivity.java
+++ app/src/main/java/net/squanchy/search/SearchActivity.java
@@ -9,31 +9,61 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.List;
 
 import net.squanchy.R;
 import net.squanchy.fonts.TypefaceStyleableActivity;
+import net.squanchy.search.model.Speaker;
+import net.squanchy.search.view.SpeakersView;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import timber.log.Timber;
 
-public class SearchActivity extends TypefaceStyleableActivity {
+public class SearchActivity extends TypefaceStyleableActivity implements SpeakersView.OnSpeakerClickedListener {
 
     private static final int SPEECH_REQUEST_CODE = 1223;
     private EditText searchField;
+    private SearchService searchService;
+    private SpeakersView speakersView;
+    private Disposable subscription;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         searchField = (EditText) findViewById(R.id.search_field);
+        speakersView = (SpeakersView) findViewById(R.id.speakers_view);
         setUpToolbar();
+
+        SearchComponent component = SearchInjector.obtain(this);
+        searchService = component.service();
     }
 
     private void setUpToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.search_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        subscription = searchService.speakers()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::onSuccess, Timber::e);
+    }
+
+    private void onSuccess(List<Speaker> speakers) {
+        speakersView.updateWith(speakers, this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        subscription.dispose();
     }
 
     @Override
@@ -57,7 +87,7 @@ public class SearchActivity extends TypefaceStyleableActivity {
         voiceSearchIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         try {
             startActivityForResult(voiceSearchIntent, SPEECH_REQUEST_CODE);
-        }catch (ActivityNotFoundException e){
+        } catch (ActivityNotFoundException e) {
             Timber.e(e);
         }
     }
@@ -69,5 +99,10 @@ public class SearchActivity extends TypefaceStyleableActivity {
             searchField.setText(results.get(0));
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSpeakerClicked(long speakerId) {
+        Toast.makeText(this, "Speaker clicked " + speakerId, Toast.LENGTH_SHORT).show();
     }
 }
