0d2784c4056052e2ca84538a534b6aee5d896a5b
Sebastiano Poggi <poggos@gmail.com>
2017-03-06 22:58:45 +0000

    Add repositories for Event and Speaker

---
+++ app/src/main/java/net/squanchy/service/repository/SpeakerRepository.java
@@ -0,0 +1,35 @@
+package net.squanchy.service.repository;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import net.squanchy.service.firebase.FirebaseDbService;
+import net.squanchy.speaker.domain.view.Speaker;
+import net.squanchy.support.lang.Checksum;
+
+import io.reactivex.Observable;
+
+import static net.squanchy.support.lang.Lists.map;
+
+public class SpeakerRepository {
+
+    private final FirebaseDbService dbService;
+    private final Checksum checksum;
+
+    public SpeakerRepository(FirebaseDbService dbService, Checksum checksum) {
+        this.dbService = dbService;
+        this.checksum = checksum;
+    }
+
+    public Observable<List<Speaker>> speakers() {
+        return dbService.speakers()
+                .map(firebaseSpeaker -> firebaseSpeaker.speakers)
+                .map(list -> map(list, firebaseSpeaker -> Speaker.create(firebaseSpeaker, checksum.getChecksumOf(firebaseSpeaker.id))))
+                .doOnNext(list -> Collections.sort(list, speakerNameComparator));
+    }
+
+    private static final Comparator<Speaker> speakerNameComparator =
+            (speaker1, speaker2) -> speaker1.name().compareToIgnoreCase(speaker2.name());
+
+}
