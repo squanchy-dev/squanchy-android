8db46ce4d235d776a7e2f705aaae199cd98fda9b
Sebastiano Poggi <poggos@gmail.com>
2017-03-13 22:12:55 +0000

    Use the NavigationModule in other components

--- app/src/main/java/net/squanchy/search/SearchInjector.java
+++ app/src/main/java/net/squanchy/search/SearchInjector.java
@@ -1,6 +1,7 @@
 package net.squanchy.search;
 
 import net.squanchy.injection.ApplicationInjector;
+import net.squanchy.navigation.NavigationModule;
 
 final class SearchInjector {
 
@@ -11,7 +12,8 @@ final class SearchInjector {
     public static SearchComponent obtain(SearchActivity activity) {
         return DaggerSearchComponent.builder()
                 .applicationComponent(ApplicationInjector.obtain(activity))
-                .searchModule(new SearchModule(activity))
+                .searchModule(new SearchModule())
+                .navigationModule(new NavigationModule(activity))
                 .build();
     }
 }
