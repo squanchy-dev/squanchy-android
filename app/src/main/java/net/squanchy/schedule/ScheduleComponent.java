cf1152b9227a8a12d61f99289aa8b17804523e37
Sebastiano Poggi <poggos@gmail.com>
2017-03-15 11:57:00 +0000

    Use NavigatorModule in ScheduleComponent

--- app/src/main/java/net/squanchy/schedule/ScheduleComponent.java
+++ app/src/main/java/net/squanchy/schedule/ScheduleComponent.java
@@ -4,12 +4,13 @@ import android.content.Context;
 
 import net.squanchy.injection.ActivityLifecycle;
 import net.squanchy.injection.ApplicationComponent;
+import net.squanchy.navigation.NavigationModule;
 import net.squanchy.navigation.Navigator;
 
 import dagger.Component;
 
 @ActivityLifecycle
-@Component(modules = {ScheduleModule.class}, dependencies = ApplicationComponent.class)
+@Component(modules = {ScheduleModule.class, NavigationModule.class}, dependencies = ApplicationComponent.class)
 public interface ScheduleComponent {
 
     ScheduleService service();
