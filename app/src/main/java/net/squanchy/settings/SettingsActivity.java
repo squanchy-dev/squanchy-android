2c098342f3252751dcc3d341c4622a01c0e071d7
Daniele Bonaldo <dany.bony@gmail.com>
2017-03-16 17:52:35 +0100

    Add toolbar to Settings activity

--- app/src/main/java/net/squanchy/settings/SettingsActivity.java
+++ app/src/main/java/net/squanchy/settings/SettingsActivity.java
@@ -1,16 +1,39 @@
 package net.squanchy.settings;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class SettingsActivity extends Activity {
+import net.squanchy.R;
+import net.squanchy.fonts.TypefaceStyleableActivity;
+
+public class SettingsActivity extends TypefaceStyleableActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_settings);
+
+        setupToolbar();
+
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.fragment_container, new SettingsFragment())
                 .commit();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
