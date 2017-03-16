f1c406dfd8f524dd2e97482fc512a323ed150b3e
Daniele Bonaldo <dany.bony@gmail.com>
2017-03-16 15:34:04 +0100

    Add SettingsFragment into activity

--- app/src/main/java/net/squanchy/settings/SettingsActivity.java
+++ app/src/main/java/net/squanchy/settings/SettingsActivity.java
@@ -1,7 +1,16 @@
 package net.squanchy.settings;
 
-import android.preference.PreferenceActivity;
+import android.app.Activity;
+import android.os.Bundle;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
 }
