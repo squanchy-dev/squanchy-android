51d87b4e662354ccbacb6c525a595b304a77fb1c
Cattaneo Stefano <stefano@nearit.com>
2017-03-16 14:16:03 +0100

    codestyle fix

--- app/src/main/java/net/squanchy/proximity/ProximityEvent.java
+++ app/src/main/java/net/squanchy/proximity/ProximityEvent.java
@@ -5,7 +5,9 @@ import com.google.auto.value.AutoValue;
 @AutoValue
 public abstract class ProximityEvent {
 
-    public static ProximityEvent create(String action, String subject) {return new AutoValue_ProximityEvent(action, subject);}
+    public static ProximityEvent create(String action, String subject) {
+        return new AutoValue_ProximityEvent(action, subject);
+    }
 
     public abstract String action();
 
