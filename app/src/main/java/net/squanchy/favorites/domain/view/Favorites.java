e52f80d9eaf0763eac1b4989741a330ca2327661
Daniele Conti <dnlcnt@gmail.com>
2017-03-17 12:25:36 +0000

    Format for the format :goat:

--- app/src/main/java/net/squanchy/favorites/domain/view/Favorites.java
+++ app/src/main/java/net/squanchy/favorites/domain/view/Favorites.java
@@ -8,6 +8,7 @@ import net.squanchy.schedule.domain.view.Event;
 
 @AutoValue
 public abstract class Favorites {
+
     public static Favorites create(List<Event> events) {
         return new AutoValue_Favorites(events);
     }
