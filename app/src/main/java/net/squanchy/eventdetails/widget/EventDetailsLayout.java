b08ea06a219a19b672d65eaa2cbf799755a8d74d
Sebastiano Poggi <poggos@gmail.com>
2017-03-18 00:12:20 +0000

    Complete event details body

--- app/src/main/java/net/squanchy/eventdetails/widget/EventDetailsLayout.java
+++ app/src/main/java/net/squanchy/eventdetails/widget/EventDetailsLayout.java
@@ -2,7 +2,7 @@ package net.squanchy.eventdetails.widget;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
+import android.text.Html;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -10,17 +10,13 @@ import android.widget.TextView;
 
 import net.squanchy.R;
 import net.squanchy.schedule.domain.view.Event;
-import net.squanchy.speaker.domain.view.Speaker;
-
-import static net.squanchy.support.lang.Lists.map;
 
 public class EventDetailsLayout extends LinearLayout {
 
-    private TextView placeView;
-    private TextView speakersView;
-    private TextView trackView;
-    private ExperienceLevelIconView experienceLevelIconView;
-    private TextView experienceLevelLabelView;
+    private static final String WHEN_DATE_TIME_FORMAT = "EEEE, d MMMM 'at' HH:mm";
+    private TextView whenTextView;
+    private TextView whereTextView;
+    private TextView descriptionTextView;
 
     public EventDetailsLayout(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
@@ -42,37 +38,23 @@ public class EventDetailsLayout extends LinearLayout {
 
         View.inflate(getContext(), R.layout.merge_event_details_layout, this);
 
-        placeView = (TextView) findViewById(R.id.place_view);
-        speakersView = (TextView) findViewById(R.id.speakers_view);
-        trackView = (TextView) findViewById(R.id.track_view);
-        experienceLevelIconView = (ExperienceLevelIconView) findViewById(R.id.experience_level_icon);
-        experienceLevelLabelView = (TextView) findViewById(R.id.experience_level_label);
+        whenTextView = (TextView) findViewById(R.id.when_text);
+        whereTextView = (TextView) findViewById(R.id.where_text);
+        descriptionTextView = (TextView) findViewById(R.id.description_text);
     }
 
     public void updateWith(Event event) {
-        updatePlaceData(event);
-        updateSpeakerData(event);
-        trackView.setVisibility(event.trackVisibility());
-        updateExperienceLevelData(event);
+        whenTextView.setText(event.startTime().toString(WHEN_DATE_TIME_FORMAT));
+        updateWhere(event);
+        updateDescription(event.description());
     }
 
-    private void updatePlaceData(Event event) {
-        placeView.setVisibility(event.placeVisibility());
-        if (event.place().isPresent()) {
-            placeView.setText(event.place().get());
-        }
+    private void updateWhere(Event event) {
+        whereTextView.setText(event.place().orNull());
     }
 
-    private void updateSpeakerData(Event event) {
-        speakersView.setVisibility(event.speakersVisibility());
-        String speakersNames = TextUtils.join(", ", map(event.speakers(), Speaker::name));
-        speakersView.setText(speakersNames);
-    }
-
-    private void updateExperienceLevelData(Event event) {
-        if (event.experienceLevel().isPresent()) {
-            experienceLevelIconView.setExperienceLevel(event.experienceLevel().get());
-            experienceLevelLabelView.setText(event.experienceLevel().get().labelStringResId());
-        }
+    private void updateDescription(String description) {
+        // TODO use Dante for this
+        descriptionTextView.setText(Html.fromHtml(description));
     }
 }
