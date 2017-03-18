1839733da1b62c401a128b8aa807d6d12bcb76ff
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-03-18 18:43:45 +0100

    create a basic ScrollListener for the RecyclerView. This will be used to load more tweets when the user reaches the (n - THRESHOLD) element of the list

---
+++ app/src/main/java/net/squanchy/tweets/view/ScrollListener.java
@@ -0,0 +1,43 @@
+package net.squanchy.tweets.view;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public abstract class ScrollListener extends RecyclerView.OnScrollListener {
+
+    private static final int THRESHOLD = 2;
+    private int previousTotalItemCount = 0;
+    private boolean loading = true;
+
+    private final LinearLayoutManager layoutManager;
+
+    public ScrollListener(LinearLayoutManager layoutManager) {
+        this.layoutManager = layoutManager;
+    }
+
+    @Override
+    public void onScrolled(RecyclerView view, int dx, int dy) {
+
+        int totalItemCount = layoutManager.getItemCount();
+        int lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition();
+
+        if (totalItemCount < previousTotalItemCount) {
+            previousTotalItemCount = totalItemCount;
+            if (totalItemCount == 0) {
+                loading = true;
+            }
+        }
+
+        if (loading && (totalItemCount > previousTotalItemCount)) {
+            loading = false;
+            previousTotalItemCount = totalItemCount;
+        }
+
+        if (!loading && (lastVisibleItemPosition + THRESHOLD) > totalItemCount) {
+            loadMore();
+            loading = true;
+        }
+    }
+
+    protected abstract void loadMore();
+}
