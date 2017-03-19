2fc9ea1bb0368f6ce1701465c5a48f92d68dc4d4
Sebastiano Poggi <poggos@gmail.com>
2017-03-19 22:58:21 +0000

    Use mapper to emit correct value

--- app/src/main/java/net/squanchy/service/firebase/FirebaseDbService.java
+++ app/src/main/java/net/squanchy/service/firebase/FirebaseDbService.java
@@ -21,7 +21,6 @@ import net.squanchy.support.lang.Func1;
 import net.squanchy.support.lang.Optional;
 
 import io.reactivex.Completable;
-import io.reactivex.Emitter;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.schedulers.Schedulers;
@@ -88,20 +87,20 @@ public final class FirebaseDbService {
     }
 
     private <T> Observable<T> observeChild(final String path, final Class<T> clazz) {
-        return observeChildAndEmit(path, clazz, Emitter::onNext);
+        return observeChildAndEmit(path, clazz, value -> value);
     }
 
     private <T> Observable<Optional<T>> observeOptionalChild(final String path, final Class<T> clazz) {
-        return observeChildAndEmit(path, clazz, (emitter, value) -> emitter.onNext(Optional.fromNullable(value)));
+        return observeChildAndEmit(path, clazz, Optional::fromNullable);
     }
 
-    private <T, V> Observable<T> observeChildAndEmit(String path, final Class<V> clazz, NextEmitter<T, V> nextEmitter) {
+    private <T, V> Observable<T> observeChildAndEmit(String path, final Class<V> clazz, Func1<V, T> valueMapper) {
         return Observable.create((ObservableEmitter<T> e) -> {
             ValueEventListener listener = new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     V value = dataSnapshot.getValue(clazz);
-                    nextEmitter.emit(e, value);
+                    e.onNext(valueMapper.call(value));
                 }
 
                 @Override
@@ -131,10 +130,5 @@ public final class FirebaseDbService {
                     .addOnSuccessListener(result -> emitter.onComplete())
                     .addOnFailureListener(emitter::onError);
         });
-    }
-
-    private interface NextEmitter<T, V> {
-
-        void emit(ObservableEmitter<T> emitter, V value);
     }
 }
