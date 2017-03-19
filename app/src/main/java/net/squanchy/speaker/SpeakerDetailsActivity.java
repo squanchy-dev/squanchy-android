2d66b1b1efc8eb34f71177304bf574f4ce7bd3d5
Sebastiano Poggi <poggos@gmail.com>
2017-03-19 12:27:45 +0000

    Support twitter and website items on speaker details

--- app/src/main/java/net/squanchy/speaker/SpeakerDetailsActivity.java
+++ app/src/main/java/net/squanchy/speaker/SpeakerDetailsActivity.java
@@ -5,13 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 
 import net.squanchy.R;
 import net.squanchy.fonts.TypefaceStyleableActivity;
 import net.squanchy.navigation.Navigator;
+import net.squanchy.speaker.domain.view.Speaker;
 import net.squanchy.speaker.widget.SpeakerDetailsLayout;
+import net.squanchy.support.lang.Optional;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -25,6 +28,8 @@ public class SpeakerDetailsActivity extends TypefaceStyleableActivity {
     private Navigator navigator;
 
     private SpeakerDetailsLayout speakerDetailsLayout;
+
+    private Optional<Speaker> speaker = Optional.absent();
 
     public static Intent createIntent(Context context, String speakerId) {
         Intent intent = new Intent(context, SpeakerDetailsActivity.class);
@@ -67,17 +72,59 @@ public class SpeakerDetailsActivity extends TypefaceStyleableActivity {
 
         subscriptions.add(service.speaker(speakerId)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(speaker -> speakerDetailsLayout.updateWith(speaker)));
+                .subscribe(this::onSpeakerRetrieved));
+    }
+
+    private void onSpeakerRetrieved(Speaker speaker) {
+        speakerDetailsLayout.updateWith(speaker);
+
+        this.speaker = Optional.of(speaker);
+        supportInvalidateOptionsMenu();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.speaker_details, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem twitterItem = menu.findItem(R.id.action_speaker_twitter);
+        MenuItem websiteItem = menu.findItem(R.id.action_speaker_website);
+
+        if (speaker.isPresent()) {
+            Speaker speaker = this.speaker.get();
+
+            twitterItem.setVisible(speaker.twitterUsername().isPresent());
+            websiteItem.setVisible(speaker.personalUrl().isPresent());
+        } else {
+            twitterItem.setVisible(false);
+            websiteItem.setVisible(false);
+        }
+
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
             finish();
+            return true;
+        } else if (itemId == R.id.action_speaker_twitter) {
+            navigate().toTwitterProfile(speaker.get().twitterUsername().get());
+            return true;
+        } else if (itemId == R.id.action_speaker_website) {
+            navigate().toExternalUrl(speaker.get().personalUrl().get());
             return true;
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    private Navigator navigate() {
+        return navigator;
     }
 
     @Override
