c9e6999491047d6ffb17468b24dde5cb71b0aa83
Sebastiano Poggi <poggos@gmail.com>
2017-03-19 19:00:32 +0000

    Extract toSpeaker()

--- app/src/main/java/net/squanchy/service/repository/SpeakerRepository.java
+++ app/src/main/java/net/squanchy/service/repository/SpeakerRepository.java
@@ -3,6 +3,7 @@ package net.squanchy.service.repository;
 import java.util.List;
 
 import net.squanchy.service.firebase.FirebaseDbService;
+import net.squanchy.service.firebase.model.FirebaseSpeaker;
 import net.squanchy.speaker.domain.view.Speaker;
 import net.squanchy.support.lang.Checksum;
 import net.squanchy.support.lang.Optional;
@@ -24,17 +25,7 @@ public class SpeakerRepository {
     public Observable<List<Speaker>> speakers() {
         return dbService.speakers()
                 .map(firebaseSpeaker -> firebaseSpeaker.speakers)
-                .map(speakers -> map(speakers, firebaseSpeaker -> Speaker.create(
-                        firebaseSpeaker.id,
-                        checksum.getChecksumOf(firebaseSpeaker.id),
-                        firebaseSpeaker.name,
-                        firebaseSpeaker.bio,
-                        Optional.fromNullable(firebaseSpeaker.company_name),
-                        Optional.fromNullable(firebaseSpeaker.company_url),
-                        Optional.fromNullable(firebaseSpeaker.personal_url),
-                        Optional.fromNullable(firebaseSpeaker.photo_url),
-                        Optional.fromNullable(firebaseSpeaker.twitter_username)
-                )));
+                .map(speakers -> map(speakers, this::toSpeaker));
     }
 
     public Observable<Speaker> speaker(String speakerId) {
@@ -42,16 +33,20 @@ public class SpeakerRepository {
                 .map(firebaseSpeakers -> firebaseSpeakers.speakers)
                 .flatMap(Observable::fromIterable)
                 .filter(firebaseSpeaker -> firebaseSpeaker.id.equals(speakerId))
-                .map(firebaseSpeaker -> Speaker.create(
-                        firebaseSpeaker.id,
-                        checksum.getChecksumOf(firebaseSpeaker.id),
-                        firebaseSpeaker.name,
-                        firebaseSpeaker.bio,
-                        Optional.fromNullable(firebaseSpeaker.company_name),
-                        Optional.fromNullable(firebaseSpeaker.company_url),
-                        Optional.fromNullable(firebaseSpeaker.personal_url),
-                        Optional.fromNullable(firebaseSpeaker.photo_url),
-                        Optional.fromNullable(firebaseSpeaker.twitter_username)
-                ));
+                .map(this::toSpeaker);
+    }
+
+    private Speaker toSpeaker(FirebaseSpeaker firebaseSpeaker) {
+        return Speaker.create(
+                firebaseSpeaker.id,
+                checksum.getChecksumOf(firebaseSpeaker.id),
+                firebaseSpeaker.name,
+                firebaseSpeaker.name,
+                Optional.fromNullable(firebaseSpeaker.company_name),
+                Optional.fromNullable(firebaseSpeaker.company_url),
+                Optional.fromNullable(firebaseSpeaker.personal_url),
+                Optional.fromNullable(firebaseSpeaker.photo_url),
+                Optional.fromNullable(firebaseSpeaker.twitter_username)
+        );
     }
 }
