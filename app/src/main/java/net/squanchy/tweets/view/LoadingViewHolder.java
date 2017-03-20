2e4ee8a3a6c1ff5085091bdee40e7d9cd962ad3c
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-03-20 22:35:15 +0100

    create the loading ViewHolder

---
+++ app/src/main/java/net/squanchy/tweets/view/LoadingViewHolder.java
@@ -0,0 +1,11 @@
+package net.squanchy.tweets.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class LoadingViewHolder extends RecyclerView.ViewHolder {
+
+    public LoadingViewHolder(View itemView) {
+        super(itemView);
+    }
+}
