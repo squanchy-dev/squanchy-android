8970cf45ff3b7e471f397863275198e87ef71ce6
Sebastiano Poggi <poggos@gmail.com>
2017-03-24 17:42:41 +0000

    Improve deeplink path-parsing code

--- app/src/main/java/net/squanchy/navigation/deeplink/DeepLinkNavigator.java
+++ app/src/main/java/net/squanchy/navigation/deeplink/DeepLinkNavigator.java
@@ -1,5 +1,8 @@
 package net.squanchy.navigation.deeplink;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.squanchy.navigation.Navigator;
 import net.squanchy.support.lang.Optional;
 
@@ -12,40 +15,49 @@ class DeepLinkNavigator {
     }
 
     void toSchedule(String path) {
-        String[] pathSegments = path.split("/");
+        List<String> pathSegments = extractSegments(path);
 
         Optional<String> dayId = Optional.absent();
-        if (pathSegments.length > 0) {
-            dayId = Optional.of(pathSegments[0]);
+        if (!pathSegments.isEmpty()) {
+            dayId = Optional.of(pathSegments.get(0));
         }
 
         Optional<String> eventId = Optional.absent();
-        if (pathSegments.length > 1) {
-            eventId = Optional.of(pathSegments[1]);
+        if (pathSegments.size() > 1) {
+            eventId = Optional.of(pathSegments.get(1));
         }
         navigator.toSchedule(dayId, eventId);
     }
 
     void toEventDetails(String path) {
-        String[] pathSegments = path.split("/");
-
-        if (pathSegments.length > 0) {
-            String eventId = pathSegments[0];
-            navigator.toEventDetails(eventId);
-        } else {
+        List<String> pathSegments = extractSegments(path);
+        if (pathSegments.isEmpty()) {
             throw DeepLinkParsingException.eventDetailsMissingEventId(path);
         }
+        
+        String eventId = pathSegments.get(0);
+        navigator.toEventDetails(eventId);
     }
 
     void toSpeakerUri(String path) {
-        String[] pathSegments = path.split("/");
-
-        if (pathSegments.length > 0) {
-            String speakerId = pathSegments[0];
-            navigator.toSpeakerDetails(speakerId);
-        } else {
+        List<String> pathSegments = extractSegments(path);
+        if (pathSegments.isEmpty()) {
             throw DeepLinkParsingException.speakerDetailsMissingSpeakerId(path);
         }
+
+        String speakerId = pathSegments.get(0);
+        navigator.toSpeakerDetails(speakerId);
+    }
+
+    private List<String> extractSegments(String path) {
+        String[] allSegments = path.split("/");
+        ArrayList<String> filteredSegments = new ArrayList<>();
+        for (String segment : allSegments) {
+            if (!segment.isEmpty()) {
+                filteredSegments.add(segment);
+            }
+        }
+        return filteredSegments;
     }
 
     void toFavorites() {
