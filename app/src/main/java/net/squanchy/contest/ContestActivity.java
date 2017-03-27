29bc82f0f47b2efe5230a39735c1c3bccee7766e
Cattaneo Stefano <stefano@nearit.com>
2017-03-27 12:36:24 +0200

    show contest recap in contest activity dialog

--- app/src/main/java/net/squanchy/contest/ContestActivity.java
+++ app/src/main/java/net/squanchy/contest/ContestActivity.java
@@ -3,24 +3,26 @@ package net.squanchy.contest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 import net.squanchy.R;
 import net.squanchy.fonts.TypefaceStyleableActivity;
-import net.squanchy.speaker.SpeakerDetailsActivity;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class ContestActivity extends TypefaceStyleableActivity {
 
     private static final String EXTRA_CHECKPOINT_ID = ContestActivity.class.getCanonicalName() + ".checkpoint_id";
 
     private ContestService contestService;
-    TextView result;
+    private TextView contestResults;
 
     public static Intent createIntent(Context context, String checkpoint) {
-        Intent intent = new Intent(context, SpeakerDetailsActivity.class);
+        Intent intent = new Intent(context, ContestActivity.class);
         intent.putExtra(EXTRA_CHECKPOINT_ID, checkpoint);
         return intent;
     }
@@ -30,9 +32,9 @@ public class ContestActivity extends TypefaceStyleableActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_contest_summary);
-        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        result = (TextView) findViewById(R.id.result);
+        contestResults = (TextView) findViewById(R.id.contest_result);
 
         ContestComponent component = ContestInjector.obtain(this);
         contestService = component.contestService();
@@ -45,11 +47,20 @@ public class ContestActivity extends TypefaceStyleableActivity {
         Intent intent = getIntent();
         String checkpointId = intent.getStringExtra(EXTRA_CHECKPOINT_ID);
 
-        contestService.updateContest(checkpointId)
+        contestService.addAchievement(checkpointId)
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this::updateWith);
     }
 
     private void updateWith(ContestStandings standings) {
-        result.setText("got results");
+        contestResults.setText(
+                String.format(Locale.US, "Checked %1$d out of %2$d stands: \n%3$s",
+                        standings.current(),
+                        standings.goal(),
+                        getContentMessage(standings.current(), standings.goal())));
+    }
+
+    private String getContentMessage(int current, int goal) {
+        return current == goal ? "Congratulation, you won!" : "Still missing " + (goal - current) + " stands";
     }
 }
