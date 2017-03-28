f5f17cc9064e313653a30ba8d5980fdf51ca6d10
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-03-28 20:46:28 +0200

    rename some methods in the TwitterService

--- app/src/main/java/net/squanchy/tweets/service/TwitterService.java
+++ app/src/main/java/net/squanchy/tweets/service/TwitterService.java
@@ -8,7 +8,6 @@ import net.squanchy.tweets.domain.view.Tweet;
 import net.squanchy.tweets.domain.view.UrlEntity;
 
 import io.reactivex.Observable;
-import io.reactivex.schedulers.Schedulers;
 
 import static net.squanchy.support.lang.Lists.map;
 
@@ -30,21 +29,21 @@ public class TwitterService {
                 .id(tweet.id)
                 .text(tweet.text)
                 .createdAt(tweet.createdAt)
-                .hashtags(mapHashtags(tweet.entities.hashtags))
-                .mentions(mapMentions(tweet.entities.userMentions))
-                .urls(mapUrls(tweet.entities.urls))
+                .hashtags(parseHashtags(tweet.entities.hashtags))
+                .mentions(parseMentions(tweet.entities.userMentions))
+                .urls(parseUrls(tweet.entities.urls))
                 .build();
     }
 
-    private List<HashtagEntity> mapHashtags(List<com.twitter.sdk.android.core.models.HashtagEntity> entities) {
+    private List<HashtagEntity> parseHashtags(List<com.twitter.sdk.android.core.models.HashtagEntity> entities) {
         return map(entities, entity -> HashtagEntity.create(entity.text, entity.getStart(), entity.getEnd()));
     }
 
-    private List<MentionEntity> mapMentions(List<com.twitter.sdk.android.core.models.MentionEntity> entities) {
+    private List<MentionEntity> parseMentions(List<com.twitter.sdk.android.core.models.MentionEntity> entities) {
         return map(entities, entity -> MentionEntity.create(entity.screenName, entity.getStart(), entity.getEnd()));
     }
 
-    private List<UrlEntity> mapUrls(List<com.twitter.sdk.android.core.models.UrlEntity> entities) {
+    private List<UrlEntity> parseUrls(List<com.twitter.sdk.android.core.models.UrlEntity> entities) {
         return map(entities, entity -> UrlEntity.create(entity.url, entity.getStart(), entity.getEnd()));
     }
 }
