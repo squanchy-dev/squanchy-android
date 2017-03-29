84045502ad356a34f63f0a228717b05b471a2255
Sebastiano Poggi <poggos@gmail.com>
2017-03-29 13:55:07 +0100

    Extract CardSpacingItemDecorator

--- app/src/main/java/net/squanchy/schedule/view/ScheduleDayPageView.java
+++ app/src/main/java/net/squanchy/schedule/view/ScheduleDayPageView.java
@@ -1,19 +1,17 @@
 package net.squanchy.schedule.view;
 
 import android.content.Context;
-import android.graphics.Rect;
 import android.support.annotation.Nullable;
-import android.support.annotation.Px;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
-import android.view.View;
 
 import java.util.List;
 
 import net.squanchy.R;
 import net.squanchy.schedule.domain.view.Event;
+import net.squanchy.support.view.CardSpacingItemDecorator;
 
 public class ScheduleDayPageView extends RecyclerView {
 
@@ -84,35 +82,6 @@ public class ScheduleDayPageView extends RecyclerView {
             Event oldEvent = oldEvents.get(oldItemPosition);
             Event newEvent = newEvents.get(newItemPosition);
             return oldEvent.equals(newEvent);
-        }
-    }
-
-    private static class CardSpacingItemDecorator extends ItemDecoration {
-
-        @Px
-        private final int horizontalSpacing;
-        @Px
-        private final int verticalSpacing;
-
-        private CardSpacingItemDecorator(@Px int horizontalSpacing, @Px int verticalSpacing) {
-            this.horizontalSpacing = horizontalSpacing;
-            this.verticalSpacing = verticalSpacing;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, State state) {
-            int position = parent.getChildAdapterPosition(view);
-            int count = state.getItemCount();
-
-            int topSpacing = verticalSpacing / 2;
-            int bottomSpacing = topSpacing;
-            if (position == 0) {
-                topSpacing = verticalSpacing;
-            } else if (position == count - 1) {
-                bottomSpacing = verticalSpacing;
-            }
-
-            outRect.set(horizontalSpacing, topSpacing, horizontalSpacing, bottomSpacing);
         }
     }
 }
--- app/src/main/java/net/squanchy/tweets/view/TweetFeedView.java
+++ app/src/main/java/net/squanchy/tweets/view/TweetFeedView.java
@@ -1,15 +1,13 @@
 package net.squanchy.tweets.view;
 
 import android.content.Context;
-import android.graphics.Rect;
 import android.support.annotation.Nullable;
-import android.support.annotation.Px;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
-import android.view.View;
 
 import net.squanchy.R;
+import net.squanchy.support.view.CardSpacingItemDecorator;
 
 public class TweetFeedView extends RecyclerView {
 
@@ -31,34 +29,5 @@ public class TweetFeedView extends RecyclerView {
         int horizontalSpacing = getResources().getDimensionPixelSize(R.dimen.card_horizontal_margin);
         int verticalSpacing = getResources().getDimensionPixelSize(R.dimen.card_vertical_margin);
         addItemDecoration(new CardSpacingItemDecorator(horizontalSpacing, verticalSpacing));
-    }
-
-    private static class CardSpacingItemDecorator extends ItemDecoration {
-
-        @Px
-        private final int horizontalSpacing;
-        @Px
-        private final int verticalSpacing;
-
-        private CardSpacingItemDecorator(@Px int horizontalSpacing, @Px int verticalSpacing) {
-            this.horizontalSpacing = horizontalSpacing;
-            this.verticalSpacing = verticalSpacing;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, State state) {
-            int position = parent.getChildAdapterPosition(view);
-            int count = state.getItemCount();
-
-            int topSpacing = verticalSpacing / 2;
-            int bottomSpacing = topSpacing;
-            if (position == 0) {
-                topSpacing = verticalSpacing;
-            } else if (position == count - 1) {
-                bottomSpacing = verticalSpacing;
-            }
-
-            outRect.set(horizontalSpacing, topSpacing, horizontalSpacing, bottomSpacing);
-        }
     }
 }
