7202559105591ebcdbff712784631464d97545da
Sebastiano Poggi <poggos@gmail.com>
2017-03-29 16:00:17 +0100

    Tweak license labels for consistency

--- app/src/main/java/net/squanchy/about/licenses/License.java
+++ app/src/main/java/net/squanchy/about/licenses/License.java
@@ -5,11 +5,11 @@ import android.support.annotation.StringRes;
 import net.squanchy.R;
 
 enum License {
-    APACHE_2("Apache 2.0", R.string.license_notice_apache_2),
-    GLIDE("BSD, part MIT and Apache 2.0", R.string.license_notice_glide),
+    APACHE_2("Apache 2.0 License", R.string.license_notice_apache_2),
+    GLIDE("BSD, part MIT and Apache 2.0 licenses", R.string.license_notice_glide),
     ECLIPSE_PUBLIC_LICENSE("Eclipse Public License 1.0", R.string.license_notice_eclipse_public_license),
-    MIT("MIT", R.string.license_notice_mit),
-    OPEN_FONT_LICENSE("Open Font License", R.string.license_notice_open_font_license);
+    MIT("MIT License", R.string.license_notice_mit),
+    OPEN_FONT_LICENSE("Open Font License 1.1", R.string.license_notice_open_font_license);
 
     private final String label;
 
