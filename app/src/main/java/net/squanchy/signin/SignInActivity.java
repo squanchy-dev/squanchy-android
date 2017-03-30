2f2072ec17cbc08a4dd5c3e6f8c6bb268e73d2c0
Sebastiano Poggi <poggos@gmail.com>
2017-03-30 14:42:30 +0100

    Implement proper sign in activity UI

--- app/src/main/java/net/squanchy/signin/SignInActivity.java
+++ app/src/main/java/net/squanchy/signin/SignInActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.Toast;
+import android.view.ViewGroup;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -23,24 +25,48 @@ public class SignInActivity extends TypefaceStyleableActivity {
     private static final int RC_SIGN_IN = 9001;
 
     private SignInService service;
-
     private GoogleApiClient googleApiClient;
+
+    private View progressView;
+    private View signInContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
-
-        findViewById(R.id.sign_in_button).setOnClickListener(view -> signIn());
 
         googleApiClient = connectToGoogleApis();
 
         SignInComponent component = SignInInjector.obtain(this);
         service = component.service();
 
+        setContentView(R.layout.activity_signin);
+
+        progressView = findViewById(R.id.progressbar);
+        signInContent = findViewById(R.id.sign_in_content);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(view -> signIn());
         findViewById(R.id.touch_outside).setOnClickListener(v -> finish());
 
-        BottomSheetBehavior.from(findViewById(R.id.bottom_sheet))
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        setBottomSheetCallbackOn(bottomSheet);
+
+        setupWindowParameters();
+    }
+
+    private GoogleApiClient connectToGoogleApis() {
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        return new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, e -> showSignInFailedError())
+                .addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions)
+                .build();
+    }
+
+    private void setBottomSheetCallbackOn(View bottomSheet) {
+        BottomSheetBehavior.from(bottomSheet)
                 .setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
                     @Override
                     public void onStateChanged(@NonNull View bottomSheet, int newState) {
@@ -58,16 +84,9 @@ public class SignInActivity extends TypefaceStyleableActivity {
                 });
     }
 
-    private GoogleApiClient connectToGoogleApis() {
-        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        return new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, e -> showSignInFailedError())
-                .addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions)
-                .build();
+    private void setupWindowParameters() {
+        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        getWindow().setGravity(Gravity.FILL_HORIZONTAL | Gravity.BOTTOM);
     }
 
     @Override
@@ -86,25 +105,28 @@ public class SignInActivity extends TypefaceStyleableActivity {
     }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        showProgressDialog();
+        showProgress();
 
         service.signInWithGoogle(account)
                 .subscribeOn(AndroidSchedulers.mainThread())
                 .subscribe(this::finish);
     }
 
+    private void showProgress() {
+        signInContent.setEnabled(false);
+        signInContent.setAlpha(.54f);
+        progressView.setVisibility(View.VISIBLE);
+    }
+
     private void showSignInFailedError() {
-        hideProgressDialog();
-        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+        hideProgress();
+        Snackbar.make(signInContent, R.string.sign_in_error_please_retry, Snackbar.LENGTH_SHORT).show();
     }
 
-    private void showProgressDialog() {
-        // TODO implement UI
-        Toast.makeText(this, "Logging you in...", Toast.LENGTH_SHORT).show();
-    }
-
-    private void hideProgressDialog() {
-        // TODO implement UI
+    private void hideProgress() {
+        signInContent.setEnabled(true);
+        signInContent.setAlpha(1f);
+        progressView.setVisibility(View.GONE);
     }
 
     private void signIn() {
