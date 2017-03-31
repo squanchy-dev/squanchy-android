113e3ec1bf96be37f896f696a5cfa91564eb3fa3
Cattaneo Stefano <stefano@nearit.com>
2017-03-31 17:23:07 +0200

    current time provider provides current local date time

--- app/src/main/java/net/squanchy/support/system/CurrentTime.java
+++ app/src/main/java/net/squanchy/support/system/CurrentTime.java
@@ -1,8 +1,14 @@
 package net.squanchy.support.system;
 
+import org.joda.time.LocalDateTime;
+
 public class CurrentTime {
 
     public Long getCurrentTimestamp() {
         return System.currentTimeMillis();
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return LocalDateTime.now();
     }
 }
