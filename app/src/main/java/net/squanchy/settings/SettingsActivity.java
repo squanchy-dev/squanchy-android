c913954a64fac50665b29110f257f899a1e088c9
Cattaneo Stefano <stefano@nearit.com>
2017-04-01 17:12:06 +0200

    propagate activity result to fragment

--- app/src/main/java/net/squanchy/settings/SettingsActivity.java
+++ app/src/main/java/net/squanchy/settings/SettingsActivity.java
@@ -1,5 +1,6 @@
 package net.squanchy.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -58,6 +59,13 @@ public class SettingsActivity extends TypefaceStyleableActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        SettingsFragment fragment = (SettingsFragment) getFragmentManager().findFragmentById(R.id.fragment_container);
+        fragment.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
