8f1fbbfd0d03e93b8bf458a593fd8d688d19cdf9
Sebastiano Poggi <poggos@gmail.com>
2017-04-01 09:53:45 +0100

    Handle first start without data

---
+++ app/src/debug/java/net/squanchy/navigation/firststart/FirstStartWithNoNetworkActivity.java
@@ -0,0 +1,7 @@
+package net.squanchy.navigation.firststart;
+
+import net.squanchy.fonts.TypefaceStyleableActivity;
+
+public class FirstStartWithNoNetworkActivity extends TypefaceStyleableActivity {
+
+}
--- app/src/main/java/net/squanchy/navigation/RoutingActivity.java
+++ app/src/main/java/net/squanchy/navigation/RoutingActivity.java
@@ -15,6 +15,9 @@ public class RoutingActivity extends TypefaceStyleableActivity {
 
     private DeepLinkRouter deepLinkRouter;
     private Navigator navigator;
+    private SignInService signInService;
+    private FirstStartPersister firstStartPersister;
+
     private Disposable subscription;
 
     @Override
@@ -24,25 +27,44 @@ public class RoutingActivity extends TypefaceStyleableActivity {
         RoutingComponent component = RoutingInjector.obtain(this);
         deepLinkRouter = component.deepLinkRouter();
         navigator = component.navigator();
-
-        SignInService signInService = component.signInService();
-        subscription = signInService.signInAnonymouslyIfNecessary().subscribe();
+        signInService = component.signInService();
+        firstStartPersister = component.firstStartPersister();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        subscription = signInService.signInAnonymouslyIfNecessary()
+                .subscribe(this::proceedToRouting, this::handleSignInError);
+    }
+
+    private void proceedToRouting() {
         Intent intent = getIntent();
         if (deepLinkRouter.hasDeepLink(intent)) {
             String intentUriString = intent.getDataString();
             Timber.i("Deeplink detected, navigating to \"%s\"", intentUriString);
             deepLinkRouter.navigateTo(intentUriString);
         } else {
-            navigator.toSchedule();
+            navigator.toHomePage();
         }
 
+        firstStartPersister.storeHasBeenStarted();
         finish();
+    }
+
+    private void handleSignInError(Throwable throwable) {
+        Timber.e(throwable, "Error while signing in on routing");
+        if (isFirstStart()) {
+            // We likely have no data here and it'd be a horrible UX, so we show a warning instead
+            // to let people know it won't work.
+            navigator.toFirstStartWithNoNetwork();
+            finish();
+        }
+    }
+
+    private boolean isFirstStart() {
+        return !firstStartPersister.hasBeenStartedAlready();
     }
 
     @Override
