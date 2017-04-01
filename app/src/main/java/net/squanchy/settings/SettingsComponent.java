37b6116b45b553750d7047ed1f642a542c508029
Cattaneo Stefano <stefano@nearit.com>
2017-04-01 17:10:36 +0200

    add proximity service dep to settings component

--- app/src/main/java/net/squanchy/settings/SettingsComponent.java
+++ app/src/main/java/net/squanchy/settings/SettingsComponent.java
@@ -4,6 +4,7 @@ import net.squanchy.injection.ActivityLifecycle;
 import net.squanchy.injection.ApplicationComponent;
 import net.squanchy.navigation.NavigationModule;
 import net.squanchy.navigation.Navigator;
+import net.squanchy.service.proximity.injection.ProximityService;
 import net.squanchy.signin.SignInModule;
 import net.squanchy.signin.SignInService;
 
@@ -16,4 +17,6 @@ public interface SettingsComponent {
     Navigator navigator();
 
     SignInService signInService();
+
+    ProximityService proximityService();
 }
