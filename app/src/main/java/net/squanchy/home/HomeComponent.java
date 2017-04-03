4e9f8a7d5a85c539779ce90eb1d0901bace8018c
Cattaneo Stefano <stefano@nearit.com>
2017-04-03 12:43:37 +0200

    add prerequisites dependency to homecomponent

--- app/src/main/java/net/squanchy/home/HomeComponent.java
+++ app/src/main/java/net/squanchy/home/HomeComponent.java
@@ -5,13 +5,17 @@ import net.squanchy.injection.ActivityLifecycle;
 import net.squanchy.injection.ApplicationComponent;
 import net.squanchy.navigation.NavigationModule;
 import net.squanchy.navigation.Navigator;
+import net.squanchy.proximity.preconditions.ProximityOptInPersister;
+import net.squanchy.proximity.preconditions.ProximityPreconditions;
+import net.squanchy.proximity.preconditions.ProximityPreconditionsModule;
+import net.squanchy.remoteconfig.RemoteConfig;
 import net.squanchy.service.proximity.injection.ProximityService;
 import net.squanchy.support.injection.CurrentTimeModule;
 
 import dagger.Component;
 
 @ActivityLifecycle
-@Component(modules = {NavigationModule.class, CurrentEventModule.class, CurrentTimeModule.class}, dependencies = ApplicationComponent.class)
+@Component(modules = {NavigationModule.class, CurrentEventModule.class, CurrentTimeModule.class, ProximityPreconditionsModule.class}, dependencies = ApplicationComponent.class)
 interface HomeComponent {
 
     Analytics analytics();
@@ -21,4 +25,10 @@ interface HomeComponent {
     Navigator navigator();
 
     CurrentEventService currentEvent();
+
+    RemoteConfig remoteConfig();
+
+    ProximityPreconditions proximityPreconditions();
+
+    ProximityOptInPersister proximityOptInPersister();
 }
--- app/src/main/java/net/squanchy/home/HomeInjector.java
+++ app/src/main/java/net/squanchy/home/HomeInjector.java
@@ -2,8 +2,13 @@ package net.squanchy.home;
 
 import android.app.Activity;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import net.squanchy.injection.ActivityContextModule;
 import net.squanchy.injection.ApplicationInjector;
+import net.squanchy.proximity.preconditions.PreconditionsRegistryModule;
+import net.squanchy.proximity.preconditions.ProximityPreconditions;
+import net.squanchy.proximity.preconditions.ProximityPreconditionsModule;
 
 final class HomeInjector {
 
@@ -11,10 +16,12 @@ final class HomeInjector {
         // no instances
     }
 
-    public static HomeComponent obtain(Activity activity) {
+    public static HomeComponent obtain(Activity activity, GoogleApiClient googleApiClient, ProximityPreconditions.Callback callback) {
         return DaggerHomeComponent.builder()
                 .applicationComponent(ApplicationInjector.obtain(activity))
                 .activityContextModule(new ActivityContextModule(activity))
+                .proximityPreconditionsModule(new ProximityPreconditionsModule(callback))
+                .preconditionsRegistryModule(new PreconditionsRegistryModule(googleApiClient))
                 .build();
     }
 }
