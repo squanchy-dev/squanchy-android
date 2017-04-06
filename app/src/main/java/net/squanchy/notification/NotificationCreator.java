8da81c2c55dadb194e126cd280f50fcce9fcfda4
Daniele Bonaldo <dany.bony@gmail.com>
2017-04-06 08:04:38 +0200

    Update Notifications background on Wear

--- app/src/main/java/net/squanchy/notification/NotificationCreator.java
+++ app/src/main/java/net/squanchy/notification/NotificationCreator.java
@@ -134,7 +134,6 @@ public class NotificationCreator {
 
         NotificationCompat.WearableExtender extender = new NotificationCompat.WearableExtender();
         extender.setBackground(BitmapFactory.decodeResource(resources, R.drawable.notification_background));
-        // TODO: update notification background
 
         return new NotificationCompat.Builder(context)
                 .setTicker(
