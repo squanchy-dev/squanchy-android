6c5da1eb6b88a8ae3e092158fb49b19163fe9e80
Daniele Conti <daniele.conti@novoda.com>
2017-04-10 16:34:24 +0100

    Extract methods

--- app/src/main/java/net/squanchy/schedule/ScheduleService.kt
+++ app/src/main/java/net/squanchy/schedule/ScheduleService.kt
@@ -8,6 +8,7 @@ import net.squanchy.schedule.domain.view.Schedule
 import net.squanchy.schedule.domain.view.SchedulePage
 import net.squanchy.service.firebase.FirebaseAuthService
 import net.squanchy.service.firebase.FirebaseDbService
+import net.squanchy.service.firebase.model.FirebaseDay
 import net.squanchy.service.firebase.model.FirebaseDays
 import net.squanchy.service.repository.EventRepository
 import org.joda.time.LocalDateTime
@@ -36,22 +37,28 @@ class ScheduleService internal constructor(
     private fun combineEventsById(): BiFunction<Map<String, List<Event>>, FirebaseDays, Schedule> {
         return BiFunction { eventsMap, (days) ->
             val pages = eventsMap.keys
-                    .map { key ->
-                        days!!.find {
-                            (id) ->
-                            id == key
-                        }
-                    }
+                    .map(findDayById(days!!))
                     .filterNotNull()
-                    .map {
-                        val date = LocalDateTime.parse(it.date!!, DATE_FORMATTER)
-                        val events = (eventsMap[it.id!!] ?: emptyList())
-                                .sortedBy { it.startTime }
-                        SchedulePage.create(it.id!!, date, events)
-                    }
+                    .map(toSchedulePage(eventsMap))
                     .sortedBy { it.date }
 
             Schedule.create(pages)
+        }
+    }
+
+    private fun findDayById(days: List<FirebaseDay>): (String) -> FirebaseDay? {
+        return { dayId ->
+            days.find { (id) ->
+                id == dayId
+            }
+        }
+    }
+
+    private fun toSchedulePage(eventsMap: Map<String, List<Event>>): (FirebaseDay) -> SchedulePage {
+        return { (id, date) ->
+            val parsedDate = LocalDateTime.parse(date!!, DATE_FORMATTER)
+            val events = (eventsMap[id!!] ?: emptyList()).sortedBy { it.startTime }
+            SchedulePage.create(id, parsedDate, events)
         }
     }
 
