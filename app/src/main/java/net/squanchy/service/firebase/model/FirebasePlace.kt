8f2205c08f6dbefa790a1432de6362f72a5dbff9
Sebastiano Poggi <poggos@gmail.com>
2017-06-04 22:37:47 +0100

    Address outstanding Detekt formatting issues

--- app/src/main/java/net/squanchy/service/firebase/model/FirebasePlace.kt
+++ app/src/main/java/net/squanchy/service/firebase/model/FirebasePlace.kt
@@ -1,5 +1,7 @@
 package net.squanchy.service.firebase.model
 
-data class FirebasePlace(var id: String? = null,
-                         var name: String? = null,
-                         var floor: String? = null)
+data class FirebasePlace(
+        var id: String? = null,
+        var name: String? = null,
+        var floor: String? = null
+)
