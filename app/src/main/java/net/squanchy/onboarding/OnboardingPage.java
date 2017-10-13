8aed7b472306258ac22b59ba7f57938ea3cb81cd
Sebastiano Poggi <poggos@gmail.com>
2017-10-13 09:16:00 +0100

    Suppress Checkstyle warning for onboarding

--- app/src/main/java/net/squanchy/onboarding/OnboardingPage.java
+++ app/src/main/java/net/squanchy/onboarding/OnboardingPage.java
@@ -2,6 +2,7 @@ package net.squanchy.onboarding;
 
 import android.app.Activity;
 
+@SuppressWarnings("NoWhitespaceBefore")     // TODO we need to fill in the pages for the onboarding
 public enum OnboardingPage {
     ;
 
