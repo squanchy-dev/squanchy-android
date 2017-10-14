2864fc15d113c266f0688072caafd2ebe471a068
Sebastiano Poggi <poggos@gmail.com>
2017-10-14 20:27:16 +0100

    Convert other AccountOnboarding files to Kotlin

---
+++ app/src/main/java/net/squanchy/onboarding/account/AccountOnboardingComponent.kt
@@ -0,0 +1,25 @@
+package net.squanchy.onboarding.account
+
+import dagger.Component
+import net.squanchy.injection.ActivityLifecycle
+import net.squanchy.injection.ApplicationComponent
+import net.squanchy.navigation.NavigationModule
+import net.squanchy.navigation.Navigator
+import net.squanchy.onboarding.Onboarding
+import net.squanchy.onboarding.OnboardingModule
+import net.squanchy.signin.SignInModule
+import net.squanchy.signin.SignInService
+
+@ActivityLifecycle
+@Component(
+        modules = arrayOf(OnboardingModule::class, SignInModule::class, NavigationModule::class),
+        dependencies = arrayOf(ApplicationComponent::class)
+)
+interface AccountOnboardingComponent {
+
+    fun onboarding(): Onboarding
+
+    fun signInService(): SignInService
+
+    fun navigator(): Navigator
+}
