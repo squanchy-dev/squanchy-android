d82a0d1e55f787f3e8fefc686d08bf2c3a3e617c
Sebastiano Poggi <poggos@gmail.com>
2017-10-16 23:01:21 +0100

    Simplify TypefaceCompat further, get rid of delegate

--- app/src/main/java/net/squanchy/support/font/TypefaceCompat.kt
+++ app/src/main/java/net/squanchy/support/font/TypefaceCompat.kt
@@ -9,24 +9,20 @@ import android.text.Spanned
 import android.text.TextPaint
 import android.text.style.MetricAffectingSpan
 
-fun CharSequence.applyTypeface(typeface: Typeface) {
-    if (this is Spannable)
-        apply { setSpan(TypefaceSpan(typeface), 0, length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE) }
-    else
-        SpannableString(this).applyTypeface(typeface)
-}
+fun CharSequence.applyTypeface(typeface: Typeface): Spanned =
+        (this as? Spannable)?.apply {
+            setSpan(TypefaceSpan(typeface), 0, length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        } ?: SpannableString(this).applyTypeface(typeface)
 
 fun Spanned.hasTypefaceSpan() = getSpans(0, length, TypefaceSpan::class.java).isNotEmpty()
 
-private class TypefaceSpan(typeface: Typeface) : MetricAffectingSpan() {
-
-    private val delegate = TypefaceDelegate(typeface)
+private class TypefaceSpan(private val typeface: Typeface) : MetricAffectingSpan() {
 
     override fun updateMeasureState(p: TextPaint) {
-        delegate.applyTypefaceTo(p)
+        p.typeface = typeface
     }
 
     override fun updateDrawState(tp: TextPaint) {
-        delegate.applyTypefaceTo(tp)
+        tp.typeface = typeface
     }
 }
