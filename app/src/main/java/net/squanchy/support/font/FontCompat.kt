a9f1fab7921b15253a2253a89f33d2a8ca453ea3
Roberto Orgiu <roberto.orgiu@gmail.com>
2017-10-16 16:30:33 +0200

    Slight refactor of the FontCompat function

--- app/src/main/java/net/squanchy/support/font/FontCompat.kt
+++ app/src/main/java/net/squanchy/support/font/FontCompat.kt
@@ -3,20 +3,24 @@
 package net.squanchy.support.font
 
 import android.content.Context
-import android.graphics.Typeface
+import android.support.annotation.FontRes
 import android.support.annotation.StyleRes
 import android.support.v4.content.res.ResourcesCompat
 import net.squanchy.R
 
-fun Context.getFontFor(@StyleRes styleResId: Int): Typeface {
-    val typeface: Typeface
+fun Context.getFontFor(@StyleRes styleResId: Int) =
+        ResourcesCompat.getFont(this, getFontIdFrom(styleResId))!!
+
+@FontRes
+private fun Context.getFontIdFrom(@StyleRes styleResId: Int): Int {
+    @FontRes val fontId: Int
+
     val typedArray = obtainStyledAttributes(styleResId, kotlin.intArrayOf(R.attr.fontFamily))
     if (typedArray.hasValue(0)) {
-        val fontId = typedArray.getResourceId(0, -1)
-        typeface = ResourcesCompat.getFont(this, fontId)!!
+        fontId = typedArray.getResourceId(0, -1)
     } else {
         error("TypedArray does not contain any fontFamily attribute!")
     }
     typedArray.recycle()
-    return typeface
+    return fontId
 }
