c2f929fd43c1b828293e4c9cd0b98c6e7c9d7480
Roberto Orgiu <roberto.orgiu@gmail.com>
2017-10-16 11:02:34 +0200

    Hopefully fix Detekt

--- app/src/main/java/net/squanchy/typeface/TypedArrayHelper.kt
+++ app/src/main/java/net/squanchy/typeface/TypedArrayHelper.kt
@@ -6,6 +6,13 @@ import android.graphics.Typeface
 import android.support.v4.content.res.ResourcesCompat
 import net.squanchy.R
 
+/**
+ * This is a workaround for when Google exposes a proper way to get a font from resources without hitting private API
+ * On Oreo, we could use TypedArray#getFont(int index), but I couldn't find the correct index for the theme and the documentation
+ * does not help. Anyway, we should scan private attributes on versions prior to Oreo, in order to get the correct ID.
+ *
+ * Please check TypedArrayHelper#obtainFont for further information (this is where we should implement the real logic)
+ */
 class TypedArrayHelper(val context: Context) {
     val font : Typeface
 
@@ -18,11 +25,6 @@ class TypedArrayHelper(val context: Context) {
         }
     }
 
-    /**
-     * This is a workaround for when Google exposes a proper way to get a font from resources without hitting private API
-     * On Oreo, we could use TypedArray#getFont(int index), but I couldn't find the correct index for the theme and the documentation
-     * does not help. Anyway, we should scan private attributes on versions prior to Oreo, in order to get the correct ID.
-     */
     private fun TypedArray.obtainFont(): Typeface =
             ResourcesCompat.getFont(context, R.font.title_typeface)!!
 
--- app/src/main/java/net/squanchy/typeface/TypefaceDelegate.kt
+++ app/src/main/java/net/squanchy/typeface/TypefaceDelegate.kt
@@ -6,17 +6,23 @@ import net.squanchy.extensions.whenTrue
 
 class TypefaceDelegate(private val newTypeface: Typeface) {
 
+    private companion object {
+        const val TEXT_SKEW_X = -.25f
+        const val FALSE_FLAG = 0
+    }
+
+
     fun applyStyleTo(paint: Paint) {
         val fakeStyle = fakeStyle(paint)
         bold(fakeStyle).whenTrue { paint.isFakeBoldText = true }
-        italic(fakeStyle).whenTrue { paint.textSkewX = -.25f }
+        italic(fakeStyle).whenTrue { paint.textSkewX = TEXT_SKEW_X }
 
         paint.typeface = newTypeface
     }
 
-    private fun bold(fakeStyle: Int) = fakeStyle.and(Typeface.BOLD) != 0
+    private fun bold(fakeStyle: Int) = fakeStyle.and(Typeface.BOLD) != FALSE_FLAG
 
-    private fun italic(fakeStyle: Int) = fakeStyle.and(Typeface.ITALIC) != 0
+    private fun italic(fakeStyle: Int) = fakeStyle.and(Typeface.ITALIC) != FALSE_FLAG
 
     private fun fakeStyle(paint: Paint) : Int {
         val oldStyle = paint.typeface?.style ?:0
