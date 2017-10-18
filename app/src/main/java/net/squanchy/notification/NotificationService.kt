75aedb6d38b4e1c4b45ee50a1d60a322abdaacff
Daniele Bonaldo <dany.bony@gmail.com>
2017-10-18 09:29:18 +0100

    Use sortedBy instead of comparatorˆ

--- app/src/main/java/net/squanchy/notification/NotificationService.kt
+++ app/src/main/java/net/squanchy/notification/NotificationService.kt
@@ -5,25 +5,17 @@ import io.reactivex.schedulers.Schedulers
 import net.squanchy.schedule.domain.view.Event
 import net.squanchy.service.firebase.FirebaseAuthService
 import net.squanchy.service.repository.EventRepository
-import java.util.Collections
-import java.util.Comparator
 
 internal class NotificationService(private val authService: FirebaseAuthService, private val eventRepository: EventRepository) {
 
     fun sortedFavourites(): Observable<List<Event>> {
         return authService.ifUserSignedInThenObservableFrom { userId ->
             eventRepository.events(userId)
-                    .map { events -> events.filter { it.favorited } }
-                    .map { events ->
-                        Collections.sort(events, byStartDate())
-                        events
-                    }
+                    .map { it.filter { it.favorited } }
+                    .map { it.sortedBy { it.startTime } }
                     .take(1)
                     .subscribeOn(Schedulers.io())
         }
     }
 
-    private fun byStartDate(): Comparator<Event> {
-        return kotlin.Comparator { event, otherEvent -> event.startTime.compareTo(otherEvent.startTime) }
-    }
 }
