641ec1ce3fad71412158ce403c0790b74afa7cbb
Vishnu Shirodkar <vishnu.shirodkar@daresay.co>
2017-11-13 18:54:33 +0100

    #333 Favorites Signed in empty layout to kotlin conversion

---
+++ app/src/main/java/net/squanchy/favorites/FavoritesSignedInEmptyLayoutPresenter.kt
@@ -0,0 +1,34 @@
+package net.squanchy.favorites
+
+import io.reactivex.Observable
+import net.squanchy.favorites.view.FavoritesSignedInEmptyLayout
+
+private const val TAPS_TO_TRIGGER_INITIAL_ACHIEVEMENT = 5
+private const val TAPS_TO_TRIGGER_PERSEVERANCE_ACHIEVEMENT = 15
+
+internal fun favoritesSignedInEmptyLayoutPresenter(clickEvent: Observable<FavoritesSignedInEmptyLayout.FavoritesClickEvent>): Observable<FavoritesEmptyViewState> {
+
+    return clickEvent.map { clickEvent ->
+
+        var counter = clickEvent.counter
+
+        var filledIcon = false
+
+        if (counter % 2 == 0) filledIcon = true
+
+        counter++
+
+        var fastLearner = false
+
+        if (counter == TAPS_TO_TRIGGER_INITIAL_ACHIEVEMENT) fastLearner = true
+
+        var perseverant = false
+
+        if (counter == TAPS_TO_TRIGGER_PERSEVERANCE_ACHIEVEMENT) perseverant = true
+
+        FavoritesEmptyViewState(counter, filledIcon, fastLearner, perseverant)
+    }
+
+}
+
+internal data class FavoritesEmptyViewState(val counter: Int, val filledIcon: Boolean, val fastLearner: Boolean, val perseverant: Boolean)
