113f75e1b4231d2467fe4f2176c47633eda133e9
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-11-15 22:22:58 +0000

    use synthetic fields

--- app/src/main/java/net/squanchy/eventdetails/widget/EventDetailsCoordinatorLayout.kt
+++ app/src/main/java/net/squanchy/eventdetails/widget/EventDetailsCoordinatorLayout.kt
@@ -2,9 +2,11 @@ package net.squanchy.eventdetails.widget
 
 import android.content.Context
 import android.support.design.widget.CoordinatorLayout
-import android.support.design.widget.FloatingActionButton
 import android.util.AttributeSet
 import android.view.View
+import kotlinx.android.synthetic.main.activity_event_details.view.favoriteFab
+import kotlinx.android.synthetic.main.activity_event_details.view.eventDetailsHeaderLayout
+import kotlinx.android.synthetic.main.activity_event_details.view.eventDetailsLayout
 
 import net.squanchy.R
 import net.squanchy.schedule.domain.view.Event
@@ -17,40 +19,25 @@ class EventDetailsCoordinatorLayout @JvmOverloads constructor(
         defStyleAttr: Int = 0
 ) : CoordinatorLayout(context, attrs, defStyleAttr) {
 
-    private lateinit var floatingActionButton: FloatingActionButton
-    private lateinit var headerLayout: EventDetailsHeaderLayout
-    private lateinit var detailsLayout: EventDetailsLayout
-
-    override fun onFinishInflate() {
-        super.onFinishInflate()
-
-        floatingActionButton = findViewById(R.id.favorite_fab)
-        headerLayout = findViewById(R.id.event_details_header)
-        detailsLayout = findViewById(R.id.event_details)
-    }
-
     internal fun updateWith(event: Event, listener: OnEventDetailsClickListener) {
-        headerLayout.updateWith(event, listener)
-        detailsLayout.updateWith(event)
+        eventDetailsHeaderLayout.updateWith(event, listener)
+        eventDetailsLayout.updateWith(event)
 
         if (canBeFavorited(event)) {
-            floatingActionButton.setImageResource(
+            favoriteFab.setImageResource(
                     if (event.favorited)
                         R.drawable.ic_favorite_filled
                     else
                         R.drawable.ic_favorite_empty
             )
-            floatingActionButton.setOnClickListener { listener.onFavoriteClick() }
-            floatingActionButton.visibility = View.VISIBLE
+            favoriteFab.setOnClickListener { listener.onFavoriteClick() }
+            favoriteFab.visibility = View.VISIBLE
         } else {
-            floatingActionButton.visibility = View.GONE
+            favoriteFab.visibility = View.GONE
         }
     }
 
-    private fun canBeFavorited(event: Event): Boolean {
-        val type = event.type
-        return type === Type.TALK || type === Type.KEYNOTE
-    }
+    private fun canBeFavorited(event: Event) = event.type === Type.TALK || event.type === Type.KEYNOTE
 
     internal interface OnEventDetailsClickListener : OnFavoriteClickListener, SpeakerView.OnSpeakerClickListener
 
