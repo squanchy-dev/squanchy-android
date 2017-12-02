f47e76c33a97da586eedffbe9205935efdc9f971
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-12-02 18:50:06 +0000

    top level function inlining for the image loader

--- app/src/main/java/net/squanchy/imageloader/ImageLoaderComponent.kt
+++ app/src/main/java/net/squanchy/imageloader/ImageLoaderComponent.kt
@@ -4,12 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import dagger.Component
 import net.squanchy.injection.ActivityContextModule
 
-fun imageLoaderComponent(activity: AppCompatActivity): ImageLoaderComponent {
-    return DaggerImageLoaderComponent.builder()
+fun imageLoaderComponent(activity: AppCompatActivity) =
+    DaggerImageLoaderComponent.builder()
         .activityContextModule(ActivityContextModule(activity))
         .imageLoaderModule(ImageLoaderModule())
         .build()
-}
 
 @Component(modules = [ImageLoaderModule::class])
 interface ImageLoaderComponent {
