7023e3c17329bab4c2a8b4b482f0f6dde3bd0108
Vishnu Shirodkar <vishnu.shirodkar@daresay.co>
2017-12-04 11:51:53 +0100

    removed wildcard import

--- app/src/main/java/net/squanchy/favorites/view/FavoritesAdapter.kt
+++ app/src/main/java/net/squanchy/favorites/view/FavoritesAdapter.kt
@@ -1,7 +1,6 @@
 package net.squanchy.favorites.view
 
 import android.content.Context
-import android.opengl.ETC1.*
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
