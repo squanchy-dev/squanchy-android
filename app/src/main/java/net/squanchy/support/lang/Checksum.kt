82cb512035bcf9b557bb387b08e9d827fb4d0be1
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2017-12-06 22:21:03 +0000

    convert the Checksum class to Kotlin

---
+++ app/src/main/java/net/squanchy/support/lang/Checksum.kt
@@ -0,0 +1,20 @@
+package net.squanchy.support.lang
+
+import java.io.UnsupportedEncodingException
+import java.util.zip.CRC32
+
+class Checksum {
+
+    private val crc32 = CRC32()
+
+    fun getChecksumOf(data: String): Long {
+        try {
+            crc32.update(data.toByteArray(charset("UTF-8")))
+            return crc32.value
+        } catch (e: UnsupportedEncodingException) {
+            throw UnsupportedOperationException("Unable to retrieve UTF-8 encoding, something fishy is going on", e)
+        } finally {
+            crc32.reset()
+        }
+    }
+}
