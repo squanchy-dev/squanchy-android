d876ff47f82329bdb24bffa15a3a788e9f000819
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2018-01-14 23:36:17 +0000

    fetch the section as a private extension property

--- app/src/main/java/net/squanchy/home/deeplink/HomeActivityIntentParser.kt
+++ app/src/main/java/net/squanchy/home/deeplink/HomeActivityIntentParser.kt
@@ -14,21 +14,22 @@ class HomeActivityIntentParser(private val savedState: Bundle?, private val inte
 
     val initialSelectedPage: BottomNavigationSection
         get() = getSectionFrom(statePersister, savedState)
-                ?: getSectionFrom(intent.extras)
+                ?: intent.extras.section
                 ?: BottomNavigationSection.SCHEDULE
 
     private fun getSectionFrom(statePersister: HomeStatePersister, savedState: Bundle?): BottomNavigationSection? {
         return statePersister.readSectionFrom(savedState)
     }
 
-    private fun getSectionFrom(intentExtras: Bundle?): BottomNavigationSection? {
-        val bundle = intentExtras ?: Bundle.EMPTY
+    private val Bundle?.section: BottomNavigationSection?
+        get() {
+            val bundle = this ?: Bundle.EMPTY
 
-        if (bundle.containsKey(KEY_INITIAL_PAGE_INDEX)) {
-            val sectionIndex = bundle.getInt(KEY_INITIAL_PAGE_INDEX)
-            return BottomNavigationSection.values()[sectionIndex]
-        } else {
-            return null
+            if (bundle.containsKey(KEY_INITIAL_PAGE_INDEX)) {
+                val sectionIndex = bundle.getInt(KEY_INITIAL_PAGE_INDEX)
+                return BottomNavigationSection.values()[sectionIndex]
+            } else {
+                return null
+            }
         }
-    }
 }
