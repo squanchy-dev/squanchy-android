22400e529d5a8159f6ce2b3acc2104e48fb45c5e
Daniele Conti <dnlcnt@gmail.com>
2018-01-14 14:54:06 +0000

    removed unused firestore db service methods, added schedule pages

--- app/src/main/java/net/squanchy/service/firestore/FirestoreDbService.kt
+++ app/src/main/java/net/squanchy/service/firestore/FirestoreDbService.kt
@@ -1,40 +1,30 @@
 package net.squanchy.service.firestore
 
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
 import io.reactivex.Observable
-import net.squanchy.service.firestore.model.FirestoreDay
-import net.squanchy.service.firestore.model.FirestoreEvent
-import net.squanchy.service.firestore.model.toObjectWithId
+import net.squanchy.service.firestore.model.schedule.FirestoreSchedulePage
 
 // TODO
 // val onCurrentThread = Executors.newSingleThreadExecutor { Thread.currentThread() }
 class FirestoreDbService(private val db: FirebaseFirestore) {
-    fun days(): Observable<List<FirestoreDay>> {
-        return Observable.create<List<FirestoreDay>> { subscriber ->
-            db.collection("days").get()
-                .addOnSuccessListener(
-                        OnSuccessListener<QuerySnapshot> {
-                            subscriber.onNext(
-                                    it.map {
-                                        it.toObjectWithId(FirestoreDay::class.java)
-                                    }
-                            )
-                        }
-                )
-                .addOnFailureListener(OnFailureListener { error -> subscriber.onError(error) })
-        }
-    }
 
-    fun event(eventId: String): Observable<FirestoreEvent> {
+    fun scheduleView(): Observable<List<FirestoreSchedulePage>> {
         return Observable.create { subscriber ->
-            db.collection("events").document(eventId).get()
-                .addOnSuccessListener { subscriber.onNext(it.toObjectWithId(FirestoreEvent::class.java)) }
-                .addOnFailureListener { subscriber.onError(it) }
-        }
+            val registration = db.collection("views")
+                .document("schedule")
+                .collection("schedule_pages")
+                .orderBy("day.date")
+                .addSnapshotListener { snapshot, exception ->
+                    if (exception != null) {
+                        subscriber.onError(exception)
+                        return@addSnapshotListener
+                    }
 
+                    subscriber.onNext(snapshot.documents.map { it.toObject(FirestoreSchedulePage::class.java) })
+                }
+
+            subscriber.setCancellable { registration.remove() }
+        }
     }
 }
 
