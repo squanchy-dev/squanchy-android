fde67060895d4a783ffae7bee8596f6d110251bc
Daniele Bonaldo <dany.bony@gmail.com>
2018-01-25 10:57:42 +0100

    Solve klint formatting issues

--- app/src/main/java/net/squanchy/support/system/CurrentTime.kt
+++ app/src/main/java/net/squanchy/support/system/CurrentTime.kt
@@ -8,5 +8,4 @@ class CurrentTime {
     private val mockTime = LocalDateTime.parse("2017-10-27T12:00:00.000")
 
     fun currentLocalDateTime(): LocalDateTime = if (BuildConfig.DEBUG) mockTime else LocalDateTime.now()
-
 }
