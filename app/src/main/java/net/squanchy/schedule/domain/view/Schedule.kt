5ee58c7699f74441bbeb16c4a5ba7875487557cb
Sebastiano Gottardo <dextorer@gmail.com>
2018-02-03 16:01:24 +0100

    Add timezone info to Schedule

--- app/src/main/java/net/squanchy/schedule/domain/view/Schedule.kt
+++ app/src/main/java/net/squanchy/schedule/domain/view/Schedule.kt
@@ -1,12 +1,14 @@
 package net.squanchy.schedule.domain.view
 
-data class Schedule(val pages: List<SchedulePage>) {
+import org.joda.time.DateTimeZone
+
+data class Schedule(val pages: List<SchedulePage>, val timezone: DateTimeZone) {
 
     val isEmpty: Boolean
         get() = pages.isEmpty()
 
     companion object {
 
-        fun create(pages: List<SchedulePage>) = Schedule(pages)
+        fun create(pages: List<SchedulePage>, timezone: DateTimeZone) = Schedule(pages, timezone)
     }
 }
