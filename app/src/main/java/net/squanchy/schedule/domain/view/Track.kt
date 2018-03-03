6fe35712f2e7817c35f7420af247155380657df2
Sebastiano Poggi <poggos@gmail.com>
2018-03-03 17:49:56 +0000

    Set default values for Track optional fields

--- app/src/main/java/net/squanchy/schedule/domain/view/Track.kt
+++ app/src/main/java/net/squanchy/schedule/domain/view/Track.kt
@@ -5,7 +5,7 @@ import net.squanchy.support.lang.Optional
 data class Track(
     val id: String,
     val name: String,
-    val accentColor: Optional<String>,
-    val textColor: Optional<String>,
-    val iconUrl: Optional<String>
+    val accentColor: Optional<String> = Optional.absent(),
+    val textColor: Optional<String> = Optional.absent(),
+    val iconUrl: Optional<String> = Optional.absent()
 )
