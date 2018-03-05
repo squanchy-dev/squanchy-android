1fb7fcce0ddf580326acc6999df41eee8ee10b29
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2018-03-05 22:40:41 +0000

    remove unused import

--- app/src/main/java/net/squanchy/service/firebase/FirebaseAuthService.kt
+++ app/src/main/java/net/squanchy/service/firebase/FirebaseAuthService.kt
@@ -4,7 +4,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.Task
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.AuthCredential
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseUser
