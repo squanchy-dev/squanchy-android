89417cc780226d74b89d617477f15e6ebd87afb8
Daniele Bonaldo <dany.bony@gmail.com>
2018-04-05 18:34:38 +0200

    Track user notification status to firebase analytics

--- app/src/main/java/net/squanchy/analytics/FirstStartDetector.kt
+++ app/src/main/java/net/squanchy/analytics/FirstStartDetector.kt
@@ -12,8 +12,17 @@ internal class FirstStartDetector(private val preferences: SharedPreferences) {
             .apply()
     }
 
+    fun isFirstStartNotificationsEnabledTracked() = preferences.getBoolean(KEY_FIRST_START_NOTIFICATIONS_ENABLED_TRACKED, false)
+
+    fun setFirstStartNotificationsEnabledTracked() {
+        preferences.edit()
+            .putBoolean(KEY_FIRST_START_NOTIFICATIONS_ENABLED_TRACKED, true)
+            .apply()
+    }
+
     companion object {
 
         private const val KEY_FIRST_START_NOT_LOGGED_IN_TRACKED = "FirstStart.not_logged_in_tracked"
+        private const val KEY_FIRST_START_NOTIFICATIONS_ENABLED_TRACKED = "FirstStart.notifications_enabled_tracked"
     }
 }
