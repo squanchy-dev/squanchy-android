ee40c7b87488ad9fe3131863bc586cbe5547c7ae
Sebastiano Poggi <poggos@gmail.com>
2018-04-07 16:08:49 +0100

    Add option() extension fun to create optionals from nullables

--- app/src/main/java/net/squanchy/support/lang/Options.kt
+++ app/src/main/java/net/squanchy/support/lang/Options.kt
@@ -3,10 +3,10 @@ package net.squanchy.support.lang
 import arrow.core.Option
 import arrow.core.getOrElse
 
-fun <T> Option<T>.or(value: T): T {
-    return this.getOrElse { value }
+fun <T> Option<T>.or(value: T): T = this.getOrElse { value }
+
+fun <T> Option<T>.getOrThrow(): T = this.getOrElse {
+    throw IllegalStateException("You must check if data is present before using get()")
 }
 
-fun <T> Option<T>.getOrThrow(): T {
-    return this.getOrElse { throw IllegalStateException("You must check if data is present before using get()") }
-}
+fun <T> T?.option(): Option<T> = Option.fromNullable(this)
