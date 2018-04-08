f60f0faeb856bafe1346a91e900c897ae3e9f2d1
Lorenzo Quiroli <lorenzo.quiroli@gmail.com>
2018-04-08 16:38:30 +0100

    delete the show current talk feature to allow the saving and restoring of the current position / day

--- app/src/main/java/net/squanchy/schedule/domain/view/Schedule.kt
+++ app/src/main/java/net/squanchy/schedule/domain/view/Schedule.kt
@@ -1,46 +1,9 @@
 package net.squanchy.schedule.domain.view
 
-import net.squanchy.support.system.CurrentTime
 import org.joda.time.DateTimeZone
-
-private const val NOT_FOUND_INDEX = -1
-private const val FIRST_PAGE_INDEX = 0
-
-private const val CURRENT_SLOT_THRESHOLD = .6f
 
 data class Schedule(val pages: List<SchedulePage>, val timeZone: DateTimeZone) {
 
     val isEmpty: Boolean
         get() = pages.all { it.events.isEmpty() }
-
-    fun findTodayIndexOrDefault(currentTime: CurrentTime): Int {
-        val now = currentTime.currentDateTime().withZone(timeZone)
-        return pages
-            .indexOfFirst { page ->
-                page.date.isEqual(now.toLocalDate())
-            }
-            .let {
-                when (it) {
-                    NOT_FOUND_INDEX -> FIRST_PAGE_INDEX
-                    else -> it
-                }
-            }
-    }
-
-    fun findNextEventForPage(page: SchedulePage, currentTime: CurrentTime) =
-        page.events
-            .firstOrNull { event ->
-                val startDateTime = event.startTime.toDateTime().withZone(event.timeZone)
-                val currentDateTime = currentTime.currentDateTime().toDateTime().withZone(event.timeZone)
-                val endDateTime = event.endTime.toDateTime().withZone(event.timeZone)
-
-                if (currentDateTime.isAfter(endDateTime)) {
-                    false
-                } else {
-                    val duration = endDateTime.millis - startDateTime.millis
-                    val offset = currentDateTime.millis - startDateTime.millis
-
-                    offset.toFloat() / duration < CURRENT_SLOT_THRESHOLD
-                }
-            }
 }
--- app/src/main/java/net/squanchy/schedule/view/ScheduleViewPagerAdapter.kt
+++ app/src/main/java/net/squanchy/schedule/view/ScheduleViewPagerAdapter.kt
@@ -15,16 +15,12 @@ class ScheduleViewPagerAdapter(context: Context) : ViewPagerAdapter<ScheduleDayP
     private lateinit var listener: (Event) -> Unit
 
     private var pages = emptyList<SchedulePage>()
-    private var initialEventForPage = emptyArray<Event?>()
-    private var triggerScrollForPage = emptyArray<((Event) -> Unit)?>()
 
     private val inflater = LayoutInflater.from(context)
 
-    fun updateWith(pages: List<SchedulePage>, initialEventForPage: Array<Event?>, listener: (Event) -> Unit) {
+    fun updateWith(pages: List<SchedulePage>, listener: (Event) -> Unit) {
         this.pages = pages
-        this.initialEventForPage = initialEventForPage
         this.listener = listener
-        this.triggerScrollForPage = arrayOfNulls(pages.size)
         notifyDataSetChanged()
     }
 
@@ -36,21 +32,12 @@ class ScheduleViewPagerAdapter(context: Context) : ViewPagerAdapter<ScheduleDayP
 
     override fun bindView(view: ScheduleDayPageView, position: Int) {
         val events = pages[position].events
-        val initialEvent = initialEventForPage[position]
-        triggerScrollForPage[position] = { view.autoscrollToEvent(events.indexOf(it), true) }
         view.updateWith(events, listener)
-        initialEvent?.let { view.autoscrollToEvent(events.indexOf(it), false) }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
         val date = pages[position].date
         return date.toString(TITLE_FORMAT_TEMPLATE).toUpperCase(Locale.getDefault())
-    }
-
-    fun getPageDayId(position: Int) = pages[position].dayId
-
-    fun refresh(page: Int, event: Event) {
-        triggerScrollForPage[page]?.invoke(event)
     }
 
     override fun isViewFromObject(view: View, anObject: Any) = view === anObject
