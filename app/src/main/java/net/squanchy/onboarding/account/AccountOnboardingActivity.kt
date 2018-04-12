f902bc05b37768e3d35c055e5e2bfb334c634e98
Sebastiano Poggi <poggos@gmail.com>
2018-04-12 19:04:17 +0100

    Avoid relying on default schedulers

--- app/src/main/java/net/squanchy/onboarding/account/AccountOnboardingActivity.kt
+++ app/src/main/java/net/squanchy/onboarding/account/AccountOnboardingActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_onboarding_account.*
 import net.squanchy.R
 import net.squanchy.navigation.Navigator
@@ -46,6 +47,7 @@ class AccountOnboardingActivity : AppCompatActivity() {
 
         disableUi()
         subscription = signInService.isSignedInToGoogle()
+            .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .timeout(SIGNIN_STATE_CHECK_TIMEOUT_SECONDS, TimeUnit.SECONDS)
             .subscribe(
