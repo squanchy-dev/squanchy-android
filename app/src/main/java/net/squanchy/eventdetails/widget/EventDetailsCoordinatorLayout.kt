e4701a58b08029e4cdfc8de9ee7ce0cff302215b
Sebastiano Poggi <poggos@gmail.com>
2018-04-14 20:43:33 +0100

    Allow workshops to be favourited

--- app/src/main/java/net/squanchy/eventdetails/widget/EventDetailsCoordinatorLayout.kt
+++ app/src/main/java/net/squanchy/eventdetails/widget/EventDetailsCoordinatorLayout.kt
@@ -3,7 +3,7 @@ package net.squanchy.eventdetails.widget
 import android.content.Context
 import android.support.design.widget.CoordinatorLayout
 import android.util.AttributeSet
-import android.view.View
+import androidx.view.isVisible
 import kotlinx.android.synthetic.main.activity_event_details.view.*
 import net.squanchy.R
 import net.squanchy.schedule.domain.view.Event
@@ -20,20 +20,18 @@ class EventDetailsCoordinatorLayout @JvmOverloads constructor(
         eventDetailsHeaderLayout.updateWith(event, listener)
         eventDetailsLayout.updateWith(event)
 
-        if (canBeFavorited(event)) {
-            favoriteFab.setImageResource(
-                    if (event.favorited)
-                        R.drawable.ic_favorite_filled
-                    else R.drawable.ic_favorite_empty
-            )
+        if (event.canBeFavorited) {
+            val favoriteImage = if (event.favorited) R.drawable.ic_favorite_filled else R.drawable.ic_favorite_empty
+            favoriteFab.setImageResource(favoriteImage)
             favoriteFab.setOnClickListener { listener.onFavoriteClick() }
-            favoriteFab.visibility = View.VISIBLE
+            favoriteFab.isVisible = true
         } else {
-            favoriteFab.visibility = View.GONE
+            favoriteFab.isVisible = false
         }
     }
 
-    private fun canBeFavorited(event: Event) = event.type === Type.TALK || event.type === Type.KEYNOTE
+    private val Event.canBeFavorited
+        get() = type == Type.TALK || type == Type.KEYNOTE || type == Type.WORKSHOP
 
     internal interface OnEventDetailsClickListener : OnFavoriteClickListener, SpeakerView.OnSpeakerClickListener
 
