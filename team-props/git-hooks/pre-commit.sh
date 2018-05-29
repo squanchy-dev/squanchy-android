4c64672b2596051f72d1196196e762dc4734d87b
sockeqwe <hannes.dorfmann@gmail.com>
2018-05-29 11:52:12 +0200

    Disable incrementabl compilation

--- team-props/git-hooks/pre-commit.sh
+++ team-props/git-hooks/pre-commit.sh
@@ -3,7 +3,7 @@
 echo "Running static analysis..."
 
 # Validate Kotlin code with detekt and KtLint before committing
-./gradlew app:detektCheck app:ktlint --daemon
+./gradlew app:detektCheck app:ktlint
 
 status=$?
 
