f03040085c92f2e83085aa04c36e675ea99adec3
Sebastiano Poggi <poggos@gmail.com>
2018-08-25 19:27:32 +0100

    Change pre-commit to pre-push hook

---
+++ team-props/git-hooks/pre-push.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Running static analysis..."
+
+# Validate Kotlin code with detekt and KtLint before committing
+./gradlew evaluateViolations --daemon
+
+status=$?
+
+if [ "$status" = 0 ] ; then
+    echo "Static analysis found no issues. Proceeding with push."
+    exit 0
+else
+    echo 1>&2 "Static analysis found issues you need to fix before pushing."
+    exit 1
+fi
