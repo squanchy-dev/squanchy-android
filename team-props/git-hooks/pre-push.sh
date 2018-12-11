ed0abddccd8c8b6fdb485f9653db806e6fd03be1
Sebastiano Poggi <poggos@gmail.com>
2018-12-11 17:03:41 +0000

    Update pre-push hook

--- team-props/git-hooks/pre-push.sh
+++ team-props/git-hooks/pre-push.sh
@@ -3,11 +3,11 @@
 echo "Running static analysis..."
 
 # Validate Kotlin code with detekt and KtLint before committing
-./gradlew evaluateViolations --daemon
+./gradlew check --daemon
 
 status=$?
 
-if [ "$status" = 0 ] ; then
+if [[ "$status" = 0 ]] ; then
     echo "Static analysis found no issues. Proceeding with push."
     exit 0
 else
