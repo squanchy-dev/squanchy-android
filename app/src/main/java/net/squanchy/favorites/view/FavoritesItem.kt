4d35be911816705ae703eadade46f2a4a1a0b386
Daniele Conti <dnlcnt@gmail.com>
2018-12-13 14:00:07 +0100

    Remove double field

--- app/src/main/java/net/squanchy/favorites/view/FavoritesItem.kt
+++ app/src/main/java/net/squanchy/favorites/view/FavoritesItem.kt
@@ -13,7 +13,6 @@ sealed class FavoritesItem {
 
     data class Header(val date: LocalDate) : FavoritesItem() {
 
-        override val id = date.atTime(LocalTime.now())
         // We're relatively sure this won't clash with event IDs
         override val id = date.atTime(LocalTime.now())
                     .atZone(ZoneId.systemDefault())
