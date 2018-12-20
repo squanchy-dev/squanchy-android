01b40a8840a002e913ff5b7ff7c9caa79c3829a5
Daniele Conti <dnlcnt@gmail.com>
2018-12-20 14:50:29 +0100

    Reformat code

--- app/src/test/java/net/squanchy/support/system/TestCurrentTime.kt
+++ app/src/test/java/net/squanchy/support/system/TestCurrentTime.kt
@@ -3,5 +3,6 @@ package net.squanchy.support.system
 import org.threeten.bp.ZonedDateTime
 
 class TestCurrentTime(private val time: ZonedDateTime) : CurrentTime {
+
     override fun currentDateTime() = time
 }
